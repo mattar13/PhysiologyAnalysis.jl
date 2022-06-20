b9a9fbbb2599c8835551c9129fb3771229c609fb
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-20 13:23:36 -0400

    added Experiments and Filtering to analysis folder

---
+++ src/Analysis/Experiments.jl
@@ -0,0 +1,172 @@
+"""
+This file contains the ABF data traces. It is a generic experiment which doesn't include any other specifics. 
+
+"""
+
+abstract type Flash{T} end 
+
+"""
+A stimulus protocol contains information about the stimulus. 
+    ### T <: Real The type declaration is the type of floating point numbers
+    ## Options are: 
+        1) type::Symbol - This is the label or type of stimulus. TODO: This will be defined in stimulus types
+        2) sweep::Int64 - The sweep number the stimulus is contained on
+        3) channel::Union{String, Int64} - The channel name or number which contains stimulus data
+        4) index_range::Tuple{Int64, Int64} - The indexes that thew stimulus starts at and the ends at
+        5) timestamps::Tuple{T, T} - The timestamps which the stimulus starts at and ends at. 
+"""
+mutable struct StimulusProtocol{T}
+    type::Symbol
+    sweep::Int64
+    channel::Union{String,Int64}
+    index_range::Tuple{Int64,Int64}
+    timestamps::Tuple{T,T}
+end
+
+function StimulusProtocol(type::Symbol, sweep::Int64, channel::Union{Int64,String}, index_range::Tuple{T,T}, t::Vector) where {T<:Real}
+    t1 = t[index_range[1]]
+    t2 = t[index_range[2]]
+    StimulusProtocol(type, sweep, channel, index_range, (t1, t2))
+end
+
+"""
+The experiment data object. 
+This contains all the data for the sweep. 
+    ### DataType is defined by {T}
+    ## The options are: 
+        1) infoDict::Dict{String,Any} -> The information object that contains all the ABF info extracted from the binary file
+        2) dt::T -> The time differential for the y axis. This is the inverse of the sampling rate collected by the digitizers
+        3) t::Vector{T} -> The y axis and timestamps for each data point.
+        4) data_array::Array{T,3} -> The data collected by the digitization process. The data array is sized by {sweeps, datapoints, channels}
+        5) chNames::Vector{String} -> The names of each channel in string format
+        6) chUnits::Vector{String} -> The units the data collected is in labeled by channel
+        7) chTelegraph::Vector{T} -> The gain on each channel.
+        8) stim_protocol::Vector{StimulusProtocol{T}} -> The stimulus protocols for each sweep. 
+"""
+mutable struct Experiment{T}
+    infoDict::Dict{String,Any}
+    dt::T
+    t::Vector{T}
+    data_array::Array{T,3}
+    chNames::Vector{String}
+    chUnits::Vector{String}
+    chTelegraph::Vector{T}
+    stim_protocol::Vector{StimulusProtocol{T}}
+end
+
+import Base: +, -, *, / #Import these basic functions to help 
++(trace::Experiment, val::Real) = trace.data_array = trace.data_array .+ val
+-(trace::Experiment, val::Real) = trace.data_array = trace.data_array .- val
+*(trace::Experiment, val::Real) = trace.data_array = trace.data_array .* val
+/(trace::Experiment, val::Real) = trace.data_array = trace.data_array ./ val
+#if the value provided is different
+function /(trace::Experiment{T}, vals::Matrix{T}) where T<:Real
+    #This function has not been worked out yet
+    if size(trace, 1) == size(vals, 1) && size(trace, 3) == size(vals, 2) #Sweeps and channels of divisor match
+        println("Both Sweeps and channels")
+    elseif size(trace, 1) == size(vals, 1) && !(size(trace, 3) == size(vals, 2))# only channels match
+        println("Only sweeps match")
+    elseif !(size(trace, 1) == size(vals, 1)) && size(trace, 3) == size(vals, 2)# only channels match
+        println("O")
+    end
+end
+
+
+import Base: size, length, getindex, setindex, sum, copy, maximum, minimum, push!, cumsum, argmin, argmax
+import Statistics.std
+
+#Extending for Experiment
+size(trace::Experiment) = size(trace.data_array)
+size(trace::Experiment, dim::Int64) = size(trace.data_array, dim)
+
+length(trace::Experiment) = size(trace, 2)
+
+#This is the basic functionality of getindex of experiments
+getindex(trace::Experiment, sweeps::Union{Int64,UnitRange{Int64}}, timepoints::Union{Int64,UnitRange{Int64}}, channels::Union{Int64,UnitRange{Int64}}) = trace.data_array[sweeps, timepoints, channels]
+#getindex(trace::Experiment, sweeps::StepRangeLen{Int64}, timepoints::StepRangeLen{Int64}, channels::StepRangeLen{Int64}) = trace[sweeps, timepoints, channels]
+
+#This function allows you to enter in a timestamp and get the data value relating to it
+function getindex(trace::Experiment, sweeps, timestamps::Union{Float64,StepRangeLen{Float64}}, channels)
+    @assert timestamps[end] .< trace.t[end] "Highest timestamp too high"
+    @assert timestamps[1] .>= trace.t[1] "Lowest timestamp too low"
+
+    if length(timestamps) == 3 #This case may have happened if a full range was not provided. 
+        timestamps = timestamps[1]:trace.dt:timestamps[end]
+    end
+    offset = -round(Int64, trace.t[1] / trace.dt)
+    timepoints = (timestamps ./ trace.dt) .+ 1
+    timepoints = (round.(Int64, timepoints))
+    timepoints .+= offset
+    trace[sweeps, timepoints, channels]
+end
+
+function getindex(trace::Experiment, sweeps, timestamps, channel::String)
+    ch_idx = findall(trace.chNames .== channel)
+    trace[sweeps, timestamps, ch_idx]
+end
+
+function getindex(trace::Experiment, sweeps, timestamps, channels::Vector{String})
+    ch_idxs = map(channel -> findall(trace.chNames .== channel)[1], channels)
+    trace[sweeps, timestamps, ch_idxs]
+end
+
+#Extending get index for Experiment
+getindex(trace::Experiment, I...) = trace.data_array[I...]
+
+setindex!(trace::Experiment, v, I...) = trace.data_array[I...] = v
+
+sum(trace::Experiment; kwargs...) = sum(trace.data_array; kwargs...)
+
+std(trace::Experiment; kwargs...) = std(trace.data_array; kwargs...)
+
+copy(nt::Experiment) = Experiment([getfield(nt, fn) for fn in fieldnames(nt |> typeof)]...)
+
+minimum(trace::Experiment; kwargs...) = minimum(trace.data_array; kwargs...)
+
+maximum(trace::Experiment; kwargs...) = maximum(trace.data_array; kwargs...)
+
+cumsum(trace::Experiment; kwargs...) = cumsum(trace.data_array; kwargs...)
+
+argmin(trace::Experiment; dims = 2) = argmin(trace.data_array, dims = dims)
+
+argmax(trace::Experiment; dims = 2) = argmax(trace.data_array, dims = dims)
+
+function push!(nt::Experiment{T}, item::AbstractArray{T}; new_name = "Unnamed") where {T<:Real}
+
+    #All of these options assume the new data point length matches the old one
+    if size(item, 2) == size(nt, 2) && size(item, 3) == size(nt, 3)
+        #item = (new_sweep, datapoints, channels)
+        nt.data_array = cat(nt.data_array, item, dims = 1)
+
+    elseif size(item, 1) == size(nt, 2) && size(item, 2) == size(nt, 3)
+        #item = (datapoints, channels) aka a single sweep
+        item = reshape(item, 1, size(item, 1), size(item, 2))
+        nt.data_array = cat(nt.data_array, item, dims = 1)
+
+    elseif size(item, 1) == size(nt, 1) && size(item, 2) == size(nt, 2)
+        #item = (sweeps, datapoints, new_channels) 
+        nt.data_array = cat(nt.data_array, item, dims = 3)
+        #Because we are adding in a new channel, add the channel name
+        push!(nt.chNames, new_name)
+
+    else
+        throw(error("File size incompatible with push!"))
+    end
+end
+
+function push!(nt_push_to::Experiment, nt_added::Experiment)
+    #push!(nt_push_to.filename, nt_added.filename...)
+    push!(nt_push_to, nt_added.data_array)
+end
+
+import Base: reverse, reverse!
+
+function reverse(trace::Experiment; kwargs...)
+    data = deepcopy(trace)
+    data.data_array = reverse(trace.data_array; kwargs...)
+    return data
+end
+
+function reverse!(trace::Experiment; kwargs...)
+    trace.data_array = reverse(trace.data_array; kwargs...)
+end
---
+++ src/Analysis/Filtering.jl
@@ -0,0 +1,372 @@
+####################These functions are for filtering and adjusting the traces################
+"""
+This function adjusts the baseline, similar to how it is done in clampfit. 
+    To change the mode of the function use the keyword argument mode
+        it can cancel baseline based on: 
+    - :mean -> the average voltage of a region
+    - :slope -> the linear slope of a region
+    To choose a region use the keyword region
+    - :prestim -> measures all time before the stimulus
+    - :whole -> measures the entire trace
+    - (start, end) -> a custom region
+It catches the baseline if the stimulus is at the beginning of the 
+    """
+function baseline_cancel(trace::Experiment; mode::Symbol = :slope, region = :prestim)
+    data = deepcopy(trace)
+    if isempty(trace.stim_protocol)
+        #println("No Stim protocol exists")
+        return data
+    else
+        for swp in 1:size(trace,1)
+            if isa(region, Tuple{Float64, Float64})
+                rng_begin = round(Int, region[1]/trace.dt)+1
+                if region[2] > trace.t[end]
+                    rng_end = length(trace.t)
+                else
+                    rng_end = round(Int, region[2]/trace.dt)+1
+                end
+            elseif isa(region, Tuple{Int64, Int64})
+                rng_begin, rng_end = region
+            elseif region == :whole
+                rng_begin = 1
+                rng_end = length(trace)
+            elseif region == :prestim
+                rng_begin = 1
+                rng_end = trace.stim_protocol[swp].index_range[1] #Get the first stimulus index
+            end
+            for ch in 1:size(trace,3)
+                if mode == :mean
+                    if (rng_end - rng_begin) != 0
+                        baseline_adjust = sum(trace.data_array[swp, rng_begin:rng_end, ch])/(rng_end-rng_begin)
+                        #Now subtract the baseline scaling value
+                        data.data_array[swp,:, ch] .= trace.data_array[swp,:,ch] .- baseline_adjust
+                    else
+                        if verbose
+                            #println("no pre-stimulus range exists")
+                        end
+                    end
+                elseif mode == :slope
+                    if (rng_end - rng_begin) != 0
+                        pfit = Polynomials.fit(trace.t[rng_begin:rng_end], trace[swp, rng_begin:rng_end , ch], 1)
+                        #Now offset the array by the linear range
+                        data.data_array[swp, :, ch] .= trace[swp, :, ch] - pfit.(trace.t)
+                    else
+                        #println("no pre-stimulus range exists")
+                    end
+                end
+            end
+        end
+        return data
+    end
+end
+
+function baseline_cancel!(trace::Experiment; mode::Symbol = :slope, region = :prestim)
+    if isempty(trace.stim_protocol)
+        #println("No stim protocol exists")
+    else
+        for swp in 1:size(trace,1)
+            if isa(region, Tuple{Float64, Float64})
+                rng_begin = round(Int, region[1]/trace.dt)+1
+                if region[2] > trace.t[end]
+                    rng_end = length(trace.t)
+                else
+                    rng_end = round(Int, region[2]/trace.dt)+1
+                end
+            elseif isa(region, Tuple{Int64, Int64})
+                rng_begin, rng_end = region
+            elseif region == :whole
+                rng_begin = 1
+                rng_end = length(trace)
+            elseif region == :prestim
+                rng_begin = 1
+                rng_end = trace.stim_protocol[swp].index_range[1] #Get the first stimulus index
+            end
+            for ch in 1:size(trace,3)
+                if mode == :mean
+                    if (rng_end - rng_begin) != 0
+                        baseline_adjust = sum(trace.data_array[swp, rng_begin:rng_end, ch])/(rng_end-rng_begin)
+                        #Now subtract the baseline scaling value
+                        trace.data_array[swp,:, ch] .= trace.data_array[swp,:,ch] .- baseline_adjust
+                    else
+                        #println("no pre-stimulus range exists")
+                    end
+                elseif mode == :slope
+                    #println(rng_begin)
+                    if (rng_end - rng_begin) != 0 # && rng_begin != 1
+                        pfit = Polynomials.fit(trace.t[rng_begin:rng_end], trace[swp, rng_begin:rng_end , ch], 1)
+                        #Now offset the array by the linear range
+                        trace.data_array[swp, :, ch] .= trace[swp, :, ch] - pfit.(trace.t)
+                    else
+                        #trace.data_array[swp, :, ch] .= trace[swp, :, ch] 
+                        #println("no pre-stimulus range exists")
+                    end
+                end
+            end
+        end
+    end
+end
+
+#TODO: Eventually I want to add all filter functions into a single function 
+
+"""
+This function applies a n-pole lowpass filter
+"""
+function lowpass_filter(trace::Experiment; freq = 40.0, pole = 8)
+    
+    responsetype = Lowpass(freq; fs =  1/trace.dt)
+    designmethod = Butterworth(8)
+    digital_filter = digitalfilter(responsetype, designmethod)
+    data = deepcopy(trace)
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+        #never adjust the stim
+            data.data_array[swp,:,ch] .= filt(digital_filter, trace[swp, :, ch])
+        end
+    end
+    return data
+end
+
+function lowpass_filter!(trace::Experiment; freq = 40.0, pole = 8)
+    
+    responsetype = Lowpass(freq; fs =  1/trace.dt)
+    designmethod = Butterworth(pole)
+    digital_filter = digitalfilter(responsetype, designmethod)
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+            trace.data_array[swp,:,ch] .= filt(digital_filter, trace[swp, :, ch])
+        end
+    end
+end
+
+lowpass_filter(trace::Experiment, freq; pole = 8) = lowpass_filter(trace; freq = freq, pole = pole)
+
+function highpass_filter(trace::Experiment; freq = 40.0, pole = 8)
+    
+    responsetype = Highpass(freq; fs =  1/trace.dt)
+    designmethod = Butterworth(8)
+    digital_filter = digitalfilter(responsetype, designmethod)
+    data = deepcopy(trace)
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+        #never adjust the stim
+            data.data_array[swp,:,ch] .= filt(digital_filter, trace[swp, :, ch])
+        end
+    end
+    return data
+end
+
+function highpass_filter!(trace::Experiment; freq = 40.0, pole = 8)
+    
+    responsetype = Highpass(freq; fs =  1/trace.dt)
+    designmethod = Butterworth(pole)
+    digital_filter = digitalfilter(responsetype, designmethod)
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+            trace.data_array[swp,:,ch] .= filt(digital_filter, trace[swp, :, ch])
+        end
+    end
+end
+
+highpass_filter(trace::Experiment, freq; pole = 8) = highpass_filter(trace; freq = freq, pole = pole)
+
+function notch_filter(trace::Experiment; center = 60.0, std = 30.0)
+    digital_filter = iirnotch(center, std, fs = 1/trace.dt)
+    data = deepcopy(trace)
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+            #never adjust the stim
+            data.data_array[swp,:,ch] .= filt(digital_filter, trace[swp, :, ch])
+        end
+    end
+    return data
+end
+
+function notch_filter!(trace::Experiment;  center = 60.0, std = 10.0)
+    digital_filter = iirnotch(center, std, fs = 1/trace.dt)
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+            trace.data_array[swp,:,ch] .= filt(digital_filter, trace[swp, :, ch])
+        end
+    end
+end
+
+function cwt_filter(trace::Experiment; wave = cDb2, β = 2, dual_window = NaiveDelta(), period_window::Tuple{Int64,Int64} = (1, 9))
+    data = deepcopy(trace)
+    for swp = 1:size(trace, 1)
+        for ch = 1:size(trace, 3)
+            c = wavelet(wave, β = β)
+            y = ContinuousWavelets.cwt(trace[swp, :, ch], c)
+            reconstruct = zeros(size(y))
+            reconstruct[:, period_window[1]:period_window[2]] .= y[:, period_window[1]:period_window[2]]
+            data.data_array[swp, :, ch] .= ContinuousWavelets.icwt(reconstruct, c, dual_window) |> vec
+        end
+    end
+    data
+end
+
+function cwt_filter!(trace::Experiment; wave = cDb2, β::Int64 = 2, period_window::Tuple{Int64, Int64} = (1,9))
+
+    for swp = 1:size(trace, 1)
+        for ch = 1:size(trace, 3)
+            c = wavelet(wave, β = β)
+            y = ContinuousWavelets.cwt(trace[swp, :, ch], c)
+            reconstruct = zeros(size(y))
+            reconstruct[:, period_window[1]:period_window[2]] .= y[:, period_window[1]:period_window[2]]
+            trace.data_array[swp, :, ch] .= ContinuousWavelets.icwt(reconstruct, c, PenroseDelta()) |> vec
+        end
+    end
+end
+
+function dwt_filter(trace::Experiment; wave = WT.db4, period_window::Tuple{Int64, Int64} = (1, 8))
+    #In this case we have to limit the analyis to the window of dyadic time
+    #This means that we can only analyze sizes if they are equal to 2^dyadic
+    data = deepcopy(trace)
+    dyad_n = trunc(Int64, log(2, size(data, 2)))
+    if period_window[2] > dyad_n
+        println("Period Window larger than dyad")
+        period_window = (period_window[1], dyad_n)
+    end
+    for swp = 1:size(data, 1), ch = 1:size(data, 3)
+        x = data[swp, 1:2^dyad_n, ch]
+        xt = dwt(x, wavelet(wave), dyad_n)
+        reconstruct = zeros(size(xt))
+        reconstruct[2^period_window[1]:2^(period_window[2])] .= xt[2^period_window[1]:2^(period_window[2])]
+        data.data_array[swp, 1:2^dyad_n, ch] .= idwt(reconstruct, wavelet(wave))
+    end
+    data
+end
+
+"""
+This is from the adaptive line interface filter in the Clampfit manual
+
+This takes notch filters at every harmonic
+"""
+function EI_filter(trace; reference_filter = 60.0, bandpass = 10.0, cycles = 5)
+    data = deepcopy(trace)
+    for cycle in 1:cycles 
+        notch_filter!(data, center = reference_filter*cycle, std = bandpass)
+    end
+    return data
+end
+
+function EI_filter!(trace; reference_filter = 60.0, bandpass = 10.0, cycles = 5)
+    for cycle in 1:cycles 
+        notch_filter!(trace, center = reference_filter*cycle, std = bandpass)
+    end
+end
+
+
+"""
+If the traces contain multiple runs, then this file averages the data
+"""
+function average_sweeps(trace::Experiment)
+    
+    data = deepcopy(trace)
+    for ch in 1:size(trace,3)
+        data[:,:,ch] .= sum(trace.data_array[:,:,ch], dims = 1)/size(trace,1)
+    end
+    return data
+end
+
+average_sweeps!(trace::Experiment) = trace.data_array = sum(trace, dims = 1)/size(trace,1) 
+
+function normalize(trace::Experiment; rng = (-1,0))
+    data = deepcopy(trace)
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+            data[swp,:,ch] .= (trace[swp,:,ch] ./ minimum(trace[swp,:,ch], dims = 2))
+        end
+    end
+    return data
+end
+
+function normalize!(trace::Experiment; rng = (-1,0))
+    for swp in 1:size(trace,1)
+        for ch in 1:size(trace,3)
+            if rng[1] < 0
+                trace.data_array[swp,:,ch] .= (trace[swp,:,ch] ./ minimum(trace[swp,:,ch], dims = 2))
+            else
+                trace.data_array[swp,:,ch] .= (trace[swp,:,ch] ./ maximum(trace[swp,:,ch], dims = 2))
+            end
+        end
+    end
+end
+
+function rolling_mean(trace::Experiment; window = 2)
+    data = deepcopy(trace)
+    for swp in 1:size(trace,1), ch in 1:size(trace,3)
+
+    end
+end
+
+################## Check these functions because they might be deprecated #####################################
+function fft_spectrum(data::Experiment)
+    #FFTW filtering
+    t = data.t
+    dt = t[2] - t[1]
+    freqs = FFTW.fftfreq(length(t), 1.0/dt) |> fftshift
+    over_0 = findall(freqs .> 0);
+    n_sweep, n_data, n_ch = size(data)
+	fft_data = zeros(Complex, n_sweep, n_data, n_ch)
+	for swp in 1:n_sweep
+		for ch in 1:n_ch
+			fft_data[swp, :, ch] = fft(data.data_array[swp, :, ch]) |> fftshift
+		end
+	end
+    return freqs[over_0], fft_data[:, over_0, :] 
+end
+
+#%% a common filter function for simplification. Remember that this is an inplace version
+function filter_data!(data::Experiment;
+        t_pre = 1.0, t_post = 4.0,
+        highpass = false, EI_bandpass = 100.0, lowpass = 300.0,
+        dwt_periods = false, #dwt_periods = (1,9),
+        cwt_periods = false, #cwt_periods = (1,9)
+    )
+    #println(t_post)
+    truncate_data!(data, t_pre = t_pre, t_post = t_post)
+    baseline_cancel!(data, mode = :slope)
+
+    #We will apply several filters consecutively
+    if highpass != false
+        highpass_filter!(data, freq = highpass) #Highpass 0.5hz
+    end
+
+    if EI_bandpass != false
+        EI_filter!(data, bandpass = EI_bandpass) #adaptive line interference according to Clampfit
+    end
+
+    if lowpass != false
+        lowpass_filter!(data, freq = lowpass) #cutout all high frequency noise
+    end
+
+    if cwt_periods != false
+        data = cwt_filter(data;
+            period_window = cwt_periods
+        )
+    end
+
+    if dwt_periods != false
+        data = dwt_filter(data;
+            period_window = dwt_periods
+        )
+    end
+
+    data * 1000
+    return data
+end
+
+function filter_data(data::Experiment; kwargs...) 
+    data_copy = deepcopy(data)
+    filter_data!(data_copy; kwargs...)
+    return data_copy
+end
+
+function filter_data(data::Tuple{Experiment{T},Experiment{T}}; kwargs...) where T <: Real
+    data_copy = deepcopy(data[1])
+    filter_data!(data_copy; kwargs...)
+    return data_copy
+end
+
+
+cone_filter(data) = filter_data(data, t_pre = 1.0, t_post = 1.0) #This should put the data right in the middle of the cone stimuli
