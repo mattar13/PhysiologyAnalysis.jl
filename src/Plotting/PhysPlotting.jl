918be660e830d8a6afd07b16fbe153cf08a8bf50
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-21 13:41:07 -0400

    need to work on PyPlot stuff

--- src/Plotting/PhysPlotting.jl
+++ src/Plotting/PhysPlotting.jl
@@ -1,4 +1,4 @@
 include("DefaultSettings.jl") #This imports all the default plotting settings for PyPlot
 include("PlottingUtilities.jl") #This imports all the plotting utilites
 include("PhysRecipes.jl")
-include("PhysPyPlot.jl") #Imports all the PyPlot utilities
+#include("PhysPyPlot.jl") #Imports all the PyPlot utilities
