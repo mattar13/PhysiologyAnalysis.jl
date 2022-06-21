20ab07336f2de0c0f46c308fbf69b76017b9ff79
Matthew Tarchick <mtarchick13@gmail.com>
2022-06-21 12:19:06 -0400

    added rmaxes, dominant time constant to the testAnalysis

--- test/testAnalysis.jl
+++ test/testAnalysis.jl
@@ -2,9 +2,20 @@ using PhysAnalysis
 
 #finding the saturated response
 print("Finding the saturated responses... ")
-@time resp = saturated_response(data);
+@time resps = saturated_response(data);
+rmaxes = maximum(resps, dims = 1); 
+
+println("Pull out the dim response")
 
 print("Finding the minima to peak... ")
 @time m2p = minima_to_peak(data);
 
 print("Finding the time to peak")
+@time t2p = time_to_peak(data);
+
+print("Finding the dominant time constant... ")
+@time t_dom = percent_recovery_interval(data, rmaxes);
+
+println("Finding the integration time")
+@time int_time = integral(data);
+
