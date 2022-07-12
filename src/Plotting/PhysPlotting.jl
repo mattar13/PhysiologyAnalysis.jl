f7cdfc2a0b827b9cf0b42892c76998a2054231b4
Matthew Tarchick <mtarchick13@gmail.com>
2022-07-12 02:19:25 -0400

    allowed PyPlot

--- src/Plotting/PhysPlotting.jl
+++ src/Plotting/PhysPlotting.jl
@@ -6,7 +6,7 @@ import PyPlot.matplotlib
 include("DefaultSettings.jl") #This imports all the default plotting settings for PyPlot
 include("PlottingUtilities.jl") #This imports all the plotting utilites
 include("PhysRecipes.jl")
-#include("PhysPyPlot.jl") #Imports all the PyPlot utilities
+include("PhysPyPlot.jl") #Imports all the PyPlot utilities
 
 export plt #Export plotting utilities
 export plot_experiment
