4969f57d8bfcb29eaa8a5b7cc568604c3bcd85e7
Matthew Tarchick <mtarchick13@gmail.com>
2022-07-30 16:07:01 -0400

    updated filtering

--- src/Filtering/filtering.jl
+++ src/Filtering/filtering.jl
@@ -204,15 +204,34 @@ function cwt_filter(trace::Experiment; wave=cDb2, β=2, dual_window=NaiveDelta()
     data
 end
 
-function cwt_filter!(trace::Experiment; wave=cDb2, β::Int64=2, period_window::Tuple{Int64,Int64}=(1, 9))
-
-    for swp = 1:size(trace, 1)
-        for ch = 1:size(trace, 3)
-            c = wavelet(wave, β=β)
+function cwt_filter!(trace::Experiment{T}; wave=cDb2, β::Int64=2, 
+    period_window::Tuple{Int64,Int64} = (1, 9), 
+    level_window::Tuple{T, T} = (-Inf, Inf),
+    return_cwt = false
+) where T <: Real
+    c = wavelet(wave, β=β)
+    if return_cwt
+        cwt = Matrix{Matrix{T}}(undef, size(trace,1), size(trace,3))
+        for swp = 1:size(trace, 1), ch = 1:size(trace, 3)
+            cwt[swp, ch] = ContinuousWavelets.cwt(trace[swp, :, ch], c)
+        end
+        return cwt
+    else
+        for swp = 1:size(trace, 1), ch = 1:size(trace, 3)
             y = ContinuousWavelets.cwt(trace[swp, :, ch], c)
             reconstruct = zeros(size(y))
-            reconstruct[:, period_window[1]:period_window[2]] .= y[:, period_window[1]:period_window[2]]
-            trace.data_array[swp, :, ch] .= ContinuousWavelets.icwt(reconstruct, c, PenroseDelta()) |> vec
+            #if period_window[end] == Inf
+            #    period_window = 
+            if !any(period_window .== -1)
+                reconstruct[:, period_window[1]:period_window[2]] .= y[:, period_window[1]:period_window[2]]
+                trace.data_array[swp, :, ch] .= ContinuousWavelets.icwt(reconstruct, c, PenroseDelta()) |> vec
+            else
+                #zero all numbers outside of the level window
+                outside_window = findall(level_window[1] .< y .< level_window[2])
+                reconstruct[:, period_window[1]:period_window[2]] .= y[:, period_window[1]:period_window[2]]
+                trace.data_array[swp, :, ch] .= ContinuousWavelets.icwt(reconstruct, c, PenroseDelta()) |> vec
+            
+            end
         end
     end
 end
@@ -395,57 +414,3 @@ end
 
 #This should put the data right in the middle of the cone stimuli(data, t_pre = 1.0, t_post = 1.0) #This should put the data right in the middle of the cone stimuli
 cone_filter(data) = filter_data(data, t_pre=1.0, t_post=1.0)
-
-"""
-These are common and typical data filtering functions
-"""
-function data_filter!(data::Experiment;
-    t_pre=1.0, t_post=4.0, truncate_based_on=:stimulus_beginning,
-    highpass=false, EI_bandpass=100.0, lowpass=300.0,
-    dwt_periods=false, #dwt_periods = (1,9),
-    cwt_periods=false #cwt_periods = (1,9)
-)
-    #println(t_post)
-    truncate_data!(data, t_pre=t_pre, t_post=t_post, truncate_based_on=truncate_based_on)
-    baseline_cancel!(data, mode=:slope)
-
-    #We will apply several filters consecutively
-    if highpass != false
-        highpass_filter!(data, freq=highpass) #Highpass 0.5hz
-    end
-
-    if EI_bandpass != false
-        EI_filter!(data, bandpass=EI_bandpass) #adaptive line interference according to Clampfit
-    end
-
-    if lowpass != false
-        lowpass_filter!(data, freq=lowpass) #cutout all high frequency noise
-    end
-
-    if cwt_periods != false
-        data = cwt_filter(data;
-            period_window=cwt_periods
-        )
-    end
-
-    if dwt_periods != false
-        data = dwt_filter(data;
-            period_window=dwt_periods
-        )
-    end
-
-    data * 1000
-    return data
-end
-
-function data_filter(data::Experiment; kwargs...)
-    data_copy = deepcopy(data)
-    data_filter!(data_copy; kwargs...)
-    return data_copy
-end
-
-function data_filter(data::Tuple{Experiment{T},Experiment{T}}; kwargs...) where {T<:Real}
-    data_copy = deepcopy(data[1])
-    data_filter!(data_copy; kwargs...)
-    return data_copy
-end
