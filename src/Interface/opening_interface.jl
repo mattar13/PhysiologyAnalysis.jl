b9b4b9b83deded4377fc7441e7828119b95bf9e0
Matthew Tarchick <mtarchick13@gmail.com>
2022-10-06 14:30:56 -0400

    updated opening interface

--- src/Interface/opening_interface.jl
+++ src/Interface/opening_interface.jl
@@ -3,5 +3,7 @@ using ePhys
 using Pluto
 
 #Eventually we will use this to determine the filtering settings
-Pluto.run(notebook = "src/Interface/filter_determination.jl")
+#Pluto.run(notebook = "src/Interface/filter_determination.jl")
+#Pluto.run(notebook = "src/Interface/experiment_analysis.jl")
+#Pluto.run(notebook = "src/Interface/subtraction_analysis.jl")
 #Pluto.run(notebook = "src/Interface/test_interface.jl")
