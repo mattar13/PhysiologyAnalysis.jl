96feea53456a4aef8d5dcbf2ffdf958839660438
Matthew Tarchick <mtarchick13@gmail.com>
2022-10-16 11:11:58 -0400

    updated interface

--- src/Interface/opening_interface.jl
+++ src/Interface/opening_interface.jl
@@ -5,6 +5,6 @@ using Pluto
 #Pluto.run(notebook = "src/Interface/test_interface.jl")
 
 #%% This section will determine filter settings
-#Pluto.run(notebook = "src/Interface/filter_determination.jl") #A-wave analysis for a file of traces
+Pluto.run(notebook = "src/Interface/filter_determination.jl") #A-wave analysis for a file of traces
 Pluto.run(notebook="src/Interface/experiment_analysis.jl") #A-wave analysis for a file of traces
 #Pluto.run(notebook = "src/Interface/subtraction_analysis.jl")
