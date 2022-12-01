47d5eaa405e64ddd90adf4a9b3df3563fcd8cbb1
Matthew Tarchick <mtarchick13@gmail.com>
2022-12-01 16:18:49 -0500

    updated

--- test/debugging.jl
+++ test/debugging.jl
@@ -8,8 +8,9 @@ ePhys.__init__()
 file = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2021_09_22_C59S-13\Mouse3_P13_C59S\BaCl_LAP4\Rods\nd4_1p_0000.abf"
 root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2022_07_11_Adult\Mouse1_Adult_WT"
 files_WT30a = "$root\\BaCl_LAP4\\Rods" |> parseABF
-@time data = readABF(files_WT30a, channels = [1]);
 @time data = data_filter(readABF(files_WT30a, channels = ["Vm_prime"]));
+data.stim_protocol 
+
 
 #%% 
 data_root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Projects\Retinoschisis" #The data root
@@ -18,6 +19,7 @@ dataset = openDatasheet(data_file, sheetName="all")
 RS_trace_B = dataset["trace_B"]
 P30_WT = (2022, 7, 11, 1, "Vm_prime4") #P30 WT (2021_9_28_n2_Vm_prime_Rods)
 q_WT30b = matchExperiment(RS_trace_B, P30_WT) |> DataFrame
+t_post_val = 2.0
 @time data_WT30a, data_WT30ab = data_filter.(readABF(q_WT30b), t_post=t_post_val)
 data_WT30B = data_WT30ab - data_WT30a
 
