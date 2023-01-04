4ea763fc9b7634b80fcb0331685a03dd68e47ca3
mattar13 <mtarchick13@gmail.com>
2023-01-04 01:56:03 -0500

    updated debugging

--- test/debugging.jl
+++ test/debugging.jl
@@ -3,23 +3,60 @@ using ePhys
 using PyPlot
 using DataFrames, Query, XLSX
 ePhys.__init__()
+import ePhys.CWTprocess
+using ContinuousWavelets
+using Statistics
+#%% enter in the file path of the file you would like to analyze here
+path = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2022_12_20_Matt_WTAdult\Mouse2_WT_Adult\BaCl_LAP4\Rods\nd3_1p_0002.abf"
+channels = ["Vm_prime"]
+data = readABF(path, channels=channels) |> average_sweeps
+baseline_adjust!(data)
+ePhys.truncate_data!(data, t_pre=0.1, t_post=4.0)
+data = data * -1
+# Look more into how the wavelets could be used
+wave = ePhys.Morlet(0.50π)
+period_window = (2^0, 2^4)
+power_window = (0.0, 1.0)
+dataRE, CWT_OG = cwt_filter(data,
+     wave=wave,
+);
 
-#%% Why is the data from wildtype looking so weird
-file_root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Paul\2019_09_24_WT-30\Mouse1_Adult_WT\BaCl_LAP4\Rods"
-files = file_root |> parseABF
-df = createDatasheet(files; filename = nothing)
-#%% Open the datafile
-data = data_filter(readABF(files, channels = ["Vm_prime", "IN 7"]), 
-     scale = 1.0, #Keep the units in mV
-     filter_channels = "Vm_prime", freq_stop = 40.0, t_pre = 0.25, t_post = 3.75, remove_global_drift = false
+dataARTIFACT, CWT_ART = cwt_filter(data,
+     wave=wave,
+     period_window=period_window, power_window=power_window
+);
+
+
+CWTo_PROC = CWTprocess(CWT_OG[1, :, :, 1])
+mu = mean(CWTo_PROC[.!isinf.(CWTo_PROC)])
+sig = std(CWTo_PROC[.!isinf.(CWTo_PROC)])
+levelsO = LinRange(mu-2*sig, mu+2*sig, 40)
+freqsO = log.(2, 1:size(CWT_OG, 3))
+
+CWTa_PROC = CWTprocess(CWT_ART[1, :, :, 1])
+mu = mean(CWTa_PROC[.!isinf.(CWTa_PROC)])
+sig = std(CWTa_PROC[.!isinf.(CWTa_PROC)])
+levelsA = LinRange(mu - 2*sig, mu + 2*sig, 40)
+freqsA = log.(2, 1:size(CWT_ART, 3))
+
+#%% Plot the results
+plt.close("all")
+figTEST, axTEST = plt.subplots(2, 2, figsize=(15, 7.4))
+imF1 = axTEST[1, 1].contourf(data.t, freqsO, CWTo_PROC,
+     cmap="seismic", levels=levelsA, extend = "both", 
 )
-#How can we downsample the data correctly
+cbar_ax1 = figTEST.add_axes([0.925, 0.5, 0.03, 0.25])
+figTEST.colorbar(imF1, cax=cbar_ax1, orientation="vertical")
+plot_experiment(axTEST[2, 1], data * -1, channels=1, c=:black)
+plot_experiment(axTEST[2, 1], dataRE, channels=1, c=:red)
 
-downsample!(data, 1/0.001)
-
-fig, ax = plt.subplots(2)
-plot_experiment(ax[1], data, channels = 1)
-plot_experiment(ax[2], data, channels = 2)
+imF2 = axTEST[1, 2].contourf(data.t, freqsA, CWTa_PROC,
+     cmap="seismic", levels=levelsA, extend = "both", 
+)
+cbar_ax2 = figTEST.add_axes([0.025, 0.5, 0.03, 0.25])
+figTEST.colorbar(imF2, cax=cbar_ax2, orientation="vertical")
+plot_experiment(axTEST[2, 2], data * -1, channels=1, c=:black)
+plot_experiment(axTEST[2, 2], dataARTIFACT * -1, channels=1, c=:red)
 
 #%% Open Pauls files
 using MAT
