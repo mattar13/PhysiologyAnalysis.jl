d026e66051ce3231537b995022744ea212ac47d1
mattar13 <mtarchick13@gmail.com>
2023-01-18 22:44:14 -0500

    updated data analysis to include dates

--- src/Datasheets/runDataAnalysis.jl
+++ src/Datasheets/runDataAnalysis.jl
@@ -19,12 +19,16 @@ updateDatasheet(datafile, all_files)
 runAnalysis(datafile)
 
 #%% Run the data analysis for the JGP files
-data_root = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Data\ERG\Restructured"
-datafile = raw"C:\Users\mtarc\OneDrive - The University of Akron\Projects\GNAT\cone_data_analysis.xlsx"
+using Dates
+Dates.now()
+yyyy = year(Dates.now())
+mm = month(Dates.now())
+dd = day(Dates.now())
+data_root  = raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Data\ERG\JGP_Files\Traces"
+datafile = "C:\\Users\\mtarc\\OneDrive - The University of Akron\\Projects\\GNAT\\$(yyyy)_$(mm)_$(dd)_cone_data_analysis.xlsx"
 all_files = data_root |> parseABF
 createDatasheet(all_files; filename=datafile, verbose=true)
 dataset = openDatasheet(datafile, sheetName="all")
 #updateDatasheet(datafile, all_files)
 resB = ePhys.run_B_wave_analysis(dataset["All_Files"])
-
 runAnalysis(datafile)
