723c4de239e2f9c8a17fbcee1a0113259a7c0a5f
mattar13 <mtarchick13@gmail.com>
2023-01-30 16:36:25 -0500

    updated opening interface

--- src/Interface/opening_interface.jl
+++ src/Interface/opening_interface.jl
@@ -5,7 +5,7 @@ using Pluto
 #Pluto.run(notebook = "src/Interface/test_interface.jl")
 
 #%% This section will determine filter settings
-#Pluto.run(notebook="src/Interface/experiment_analysis.jl")
-#Pluto.run(notebook="src/Interface/file_analysis.jl") #A-wave analysis for a file of traces
+Pluto.run(notebook="src/Interface/experiment_analysis.jl")
+Pluto.run(notebook="src/Interface/file_analysis.jl") #A-wave analysis for a file of traces
 Pluto.run(notebook = "src/Interface/filter_determination.jl") #A-wave analysis for a file of traces
-#Pluto.run(notebook = "src/Interface/subtraction_analysis.jl")
+Pluto.run(notebook = "src/Interface/subtraction_analysis.jl")
