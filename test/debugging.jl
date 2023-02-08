4d69a5bd858c74bb29a3231c6759b9ff9dcca953
mattar13 <mtarchick13@gmail.com>
2023-02-07 22:02:56 -0500

    emptied out debugging

--- test/debugging.jl
+++
@@ -1,9 +0,0 @@
-using Revise, ePhys
-using DataFrames, Query, XLSX
-import ePhys: run_A_wave_analysis, run_B_wave_analysis, run_G_wave_analysis
-exp_root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2022_07_11_Adult\Mouse3_Adult_RS1KO"
-experiment_paths = exp_root |> parseABF
-
-all_files = createDatasheet(experiment_paths; filename = savefile_name)
-
-res_A = run_A_wave_analysis(all_files)
