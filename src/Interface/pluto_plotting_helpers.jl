109c6543407eec5dc21f2521bbeb701ffe11b186
mattar13 <mtarchick13@gmail.com>
2023-02-22 00:37:49 -0500

    making some pluto plotting helpers

---
+++ src/Interface/pluto_plotting_helpers.jl
@@ -0,0 +1,8 @@
+function plot_data_summary(ax, data, qTrace; mode = :A)
+     if length(data.chNames) == 1 #Only plot one row
+          plot_experiment(ax[1], data)
+     else #plot multiple rows
+
+
+     end
+end
