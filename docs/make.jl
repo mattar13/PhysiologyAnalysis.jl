7e1e56177d276504f15b43d4cc63bd85b490d115
mattar13 <mtarchick13@gmail.com>
2023-02-28 16:26:22 -0500

    committing documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,14 @@
-using Documenter, ePhys
+using Documenter, PhysiologyAnalysis
+
+makedocs(
+     sitename = "PhysiologyAnalysis.jl",
+     #modules = [ PhysiologyAnalysis ],
+     #pages = [
+     #     "Home" => "index.md"
+     #]
+)
+
+#deploydocs(
+     #repo = "github.com/mattar13/PhysiologyAnalysis.jl.git",
+     #target = "build"
+#)
