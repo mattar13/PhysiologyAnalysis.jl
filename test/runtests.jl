42b7e478bfa431a8607c726fbea47ddd3bdb8db6
mattar13 <mtarchick13@gmail.com>
2023-03-01 23:17:42 -0500

    updating tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
-using NeuroAnalysis
+using ElectroPhysiology
+using PhysiologyAnalysis
 using Test
 
-@testset "NeuroAnalysis.jl" begin
-    # Write your tests here.
+test_file = raw"to_analyze.abf"
+data = readABF(test_file)
+@testset "Testing ElectroPhysiology" begin
+    @test !isnothing(data)
+    @test isa(data, ElectroPhysiology.Experiment)
+end
+
+@testset "Testing data filtering" begin
+    data_filtered = filter_data(data)
+    @test 
+
+
 end
