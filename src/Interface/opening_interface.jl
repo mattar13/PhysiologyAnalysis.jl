b216a9da7436a0832026f19ae144fa22e8b592ab
mattar13 <mtarchick13@gmail.com>
2023-03-01 23:45:01 -0500

    removed interface (to it's own package)

--- src/Interface/opening_interface.jl
+++
@@ -1,6 +0,0 @@
-#Pluto.run(notebook = "src/Interface/test_interface.jl")
-run_experiment_analysis() = Pluto.run(notebook="src/Interface/experiment_analysis.jl")
-run_datasheet_analysis() = Pluto.run(notebook="src/Interface/datasheet_analysis.jl")
-run_trace_analysis() = Pluto.run(notebook="src/Interface/file_analysis.jl") #A-wave analysis for a file of traces
-run_filter_determination() = Pluto.run(notebook = "src/Interface/filter_determination.jl") #A-wave analysis for a file of traces
-run_subtraction_analysis() = Pluto.run(notebook = "src/Interface/subtraction_analysis.jl")
