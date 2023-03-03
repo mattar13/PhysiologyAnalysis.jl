8170242be5fb548ce9bda19b13f657f7b2730399
mattar13 <mtarchick13@gmail.com>
2023-03-03 00:45:14 -0500

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -8,4 +8,4 @@ import PhysiologyAnalysis: DataFrame
 testRoot = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2023_02_23_MattR141C\Mouse1_Adult_R141C"
 dataset = createDataset(testRoot)
 dataset["STATS"] = dataset_statistics(dataset)
-dataset["EXPERIMENTS"]
+dataset["TRACES"]
