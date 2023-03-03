6d66faf70fdd325654dce07145585f7f3d605c1d
mattar13 <mtarchick13@gmail.com>
2023-03-03 12:04:51 -0500

    updated all revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -3,9 +3,12 @@ using Revise
 using PhysiologyAnalysis
 import PhysiologyAnalysis: readABF, parseABF
 import PhysiologyAnalysis: DataFrame
+
 #%% Add the ability to flag files and remove them from the analysis
 
-testRoot = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2023_02_23_MattR141C\Mouse1_Adult_R141C"
-dataset = createDataset(testRoot)
+data_root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis" #The data root |> parseABF
+datafile = "C:\\Users\\mtarc\\OneDrive - The University of Akron\\Projects\\Retinoschisis\\data_analysis.xlsx"
+all_files = openDataset(datafile, sheetnames = "ALL_FILES")
+dataset = runTraceAnalysis(all_files)
 dataset["STATS"] = dataset_statistics(dataset)
-dataset["TRACES"]
+saveDataset(dataset, all_files)
