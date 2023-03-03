4aa2b3ac9a76829955fa3c936295c4a2e8080f67
mattar13 <mtarchick13@gmail.com>
2023-03-02 22:13:28 -0500

    added HypothesisTests and sig_symbol

--- src/Analysis/Stats.jl
+++ src/Analysis/Stats.jl
@@ -16,65 +16,115 @@ function RSQ(ŷ::Array{T}, y::Array{T}) where {T<:Real}
      1 - SSE / SST
 end
 
-rolling_mean(arr::AbstractArray; radius = 5) = [sum(arr[i:i+radius]) / radius for i = 1:length(arr)-radius]
-
-
-"""
-This function calculates the standard error of the Mean and is safe for querying
-"""
-sem(x) = std(x) / sqrt(length(x))
-
-#=
-ENV["R_HOME"] = raw"C:\Program Files\R\R-4.1.2"
-#Have to point to the correct R path first
-using RCall #used for calculating Two-Way ANOVAs
-"""
-#This function calls R and computes Two-Way ANOVA
-#How to call R objects
-R"x = 2"
-#How to retrive R objects
-@rget x
-#How to send objects to R
-z = 10.0
-@rput z
-"""
-function R_ANOVA(data::DataFrame)
-     #install.packages("dplyr")
-     r = RObject(data) #This inserts the julia DataFrame into R
-     #convert Genotype into factor
-     @rput r
-     R"""
-     r <- as.data.frame(lapply(r, unlist))#need to convert the dataframe into a new type
-     #We need to convert the Age, Genotype and Photons to factor
-     r$Genotype <- factor(r$Genotype)
-     r$Age <- factor(r$Age) 
-     #Generate a frequency table
-     freq_table <- table(r$Genotype, r$Age)
-     resAOV2 <- aov(Rmax ~ Genotype:Age, data = r)
-     summaryAOV2 <- summary(resAOV2)
-     resTUKEY <- TukeyHSD(resAOV2)
-     """
-     freq_table = @rget freq_table #get the frequency table back
-     summaryAOV2 = @rget summaryAOV2
-     resAOV2 = @rget resAOV2
-     resTUKEY = @rget resTUKEY
-     return (freq_table, summaryAOV2, resAOV2, resTUKEY)
+function sig_symbol(val)
+     if val <= 0.001
+          return "***"
+     elseif val <= 0.005
+          return "**"
+     elseif val <= 0.05
+          return "*"
+     else
+          return "-"
+     end
 end
 
+function dataset_statistics(qEXP; control = "WT")
+     res_rmax = qEXP |> @groupby({_.Genotype, _.Age}) |> 
+          @map({Genotype = key(_)[1], Age = key(_)[2],
+               N = length(_),
+               AVG = mean(_.rmax), STD = std(_.rmax), SEM = sem(_.rmax), CI = 1.96*sem(_.rmax), 
+               LOWER = mean(_.rmax) - 1.96*sem(_.rmax), UPPER = mean(_.rmax) + 1.96*sem(_.rmax),
+               P = 0.0, SIGN = "-"
+          })|> @orderby(_.Age) |> @thenby(_.Genotype) |> DataFrame
 
-function R_T_TEST(data::DataFrame)
-     r = RObject(data) #This inserts the julia DataFrame into R
-     #convert Genotype into factor
-     @rput r
+     res_rdim = qEXP |> @groupby({_.Genotype, _.Age}) |> 
+          @map({Genotype = key(_)[1], Age = key(_)[2],
+               N = length(_),
+               AVG = mean(_.rdim), STD = std(_.rdim), SEM = sem(_.rdim), CI = 1.96*sem(_.rdim), 
+               LOWER = mean(_.rdim) - 1.96*sem(_.rdim), UPPER = mean(_.rdim) + 1.96*sem(_.rdim),
+               P = 0.0, SIGN = "-"
+          })|> @orderby(_.Age) |> @thenby(_.Genotype) |> DataFrame
 
-     R"""
-         r <- as.data.frame(lapply(r, unlist))#need to convert the dataframe into a new type
-         #We need to convert the Age, Genotype and Photons to factor
-         r$Genotype <- factor(r$Genotype)
-         freq_table <- table(r$Genotype, r$Age)
-         pairTTest <- pairwise.t.test(r$Rmax, r$Genotype, p.adjust.method = "BH")
-     """
-     ptt = @rget pairTTest
-     return ptt
+     res_K_fit = qEXP |> @groupby({_.Genotype, _.Age}) |> 
+          @map({Genotype = key(_)[1], Age = key(_)[2],
+               N = length(_),
+               AVG = mean(_.K_fit), STD = std(_.K_fit), SEM = sem(_.K_fit), CI = 1.96*sem(_.K_fit), 
+               LOWER = mean(_.K_fit) - 1.96*sem(_.K_fit), UPPER = mean(_.K_fit) + 1.96*sem(_.K_fit),
+               P = 0.0, SIGN = "-"
+          })|> @orderby(_.Age) |> @thenby(_.Genotype) |> DataFrame
+
+     res_integration_time = qEXP |> @groupby({_.Genotype, _.Age}) |> 
+          @map({Genotype = key(_)[1], Age = key(_)[2],
+               N = length(_),
+               AVG = mean(_.integration_time), STD = std(_.integration_time), SEM = sem(_.integration_time), CI = 1.96*sem(_.integration_time), 
+               LOWER = mean(_.integration_time) - 1.96*sem(_.integration_time), UPPER = mean(_.integration_time) + 1.96*sem(_.integration_time),
+               P = 0.0, SIGN = "-"
+          })|> @orderby(_.Age) |> @thenby(_.Genotype) |> DataFrame
+     res_time_to_peak = qEXP |> @groupby({_.Genotype, _.Age}) |> 
+          @map({Genotype = key(_)[1], Age = key(_)[2],
+               N = length(_),
+               AVG = mean(_.time_to_peak), STD = std(_.time_to_peak), SEM = sem(_.time_to_peak), CI = 1.96*sem(_.time_to_peak), 
+               LOWER = mean(_.time_to_peak) - 1.96*sem(_.time_to_peak), UPPER = mean(_.time_to_peak) + 1.96*sem(_.time_to_peak),
+               P = 0.0, SIGN = "-"
+          })|> @orderby(_.Age) |> @thenby(_.Genotype) |> DataFrame
+     res_percent_recovery = qEXP |> @groupby({_.Genotype, _.Age}) |> 
+          @map({Genotype = key(_)[1], Age = key(_)[2],
+               N = length(_),
+               AVG = mean(_.percent_recovery), STD = std(_.percent_recovery), SEM = sem(_.percent_recovery), CI = 1.96*sem(_.percent_recovery), 
+               LOWER = mean(_.percent_recovery) - 1.96*sem(_.percent_recovery), UPPER = mean(_.percent_recovery) + 1.96*sem(_.percent_recovery),
+               P = 0.0, SIGN = "-"
+          })|> @orderby(_.Age) |> @thenby(_.Genotype) |> DataFrame
+     
+     for (idx, info) in enumerate(eachrow(res_rmax))
+          #first load the control data
+          ctrl_data = qEXP |> @filter(_.Age == info.Age && _.Genotype == control) |> DataFrame
+          exp_data = qEXP |> @filter(_.Age == info.Age && _.Genotype == info.Genotype) |> DataFrame
+          #println(size(ctrl_data))
+          #println(size(exp_data))
+          if size(exp_data,1) > 1
+               pvalue_rmax = UnequalVarianceTTest(ctrl_data.rmax, exp_data.rmax) |> pvalue
+               pvalue_rdim = UnequalVarianceTTest(ctrl_data.rdim, exp_data.rdim) |> pvalue
+               pvalue_K_fit = UnequalVarianceTTest(ctrl_data.K_fit, exp_data.K_fit) |> pvalue
+               pvalue_integration_time = UnequalVarianceTTest(ctrl_data.integration_time, exp_data.integration_time) |> pvalue
+               pvalue_time_to_peak = UnequalVarianceTTest(ctrl_data.time_to_peak, exp_data.time_to_peak) |> pvalue
+               pvalue_percent_recovery = UnequalVarianceTTest(ctrl_data.percent_recovery, exp_data.percent_recovery) |> pvalue
+
+               sig_rmax = sig_symbol(pvalue_rmax)
+               sig_rdim = sig_symbol(pvalue_rdim)
+               sig_K_fit = sig_symbol(pvalue_K_fit)
+               sig_integration_time = sig_symbol(pvalue_integration_time)
+               sig_time_to_peak = sig_symbol(pvalue_time_to_peak)
+               sig_percent_recovery = sig_symbol(pvalue_percent_recovery)
+
+               #println("Test $(idx) : $(info.Age) $(info.Genotype) = $(res_Pval) $(SIG_SYMBOL)")
+               res_rmax[idx, :P] = pvalue_rmax
+               res_rdim[idx, :P] = pvalue_rdim
+               res_K_fit[idx, :P] = pvalue_K_fit
+               res_integration_time[idx, :P] = pvalue_integration_time
+               res_time_to_peak[idx, :P] = pvalue_time_to_peak
+               res_percent_recovery[idx, :P] = pvalue_percent_recovery
+
+               res_rmax[idx, :SIGN] = sig_rmax
+               res_rdim[idx, :SIGN] = sig_rdim
+               res_K_fit[idx, :SIGN] = sig_K_fit
+               res_integration_time[idx, :SIGN] = sig_integration_time
+               res_time_to_peak[idx, :SIGN] = sig_time_to_peak
+               res_percent_recovery[idx, :SIGN] = sig_percent_recovery
+          else
+               res_rmax[idx, :P] = 1.0
+               res_rdim[idx, :P] = 1.0
+               res_K_fit[idx, :P] = 1.0
+               res_integration_time[idx, :P] = 1.0
+               res_time_to_peak[idx, :P] = 1.0
+               res_percent_recovery[idx, :P] = 1.0
+
+               res_rmax[idx, :SIGN] = "-"
+               res_rdim[idx, :SIGN] = "-"
+               res_K_fit[idx, :SIGN] = "-"
+               res_integration_time[idx, :SIGN] = "-"
+               res_time_to_peak[idx, :SIGN] = "-"
+               res_percent_recovery[idx, :SIGN] = "-"
+          end
+     end
+     return res_rmax, res_rdim, res_K_fit, res_integration_time, res_time_to_peak, res_percent_recovery
 end
-=#
