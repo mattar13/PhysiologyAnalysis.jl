ed9ad8695a942d44f2b94a126e56b6102beb56a0
mattar13 <mtarchick13@gmail.com>
2023-03-02 22:49:48 -0500

    keeping revisison section

--- test/revisions.jl
+++ test/revisions.jl
@@ -1,9 +1,11 @@
 #%% Section 1. Revision of interface
 using Revise
-using ElectroPhysiology
 using PhysiologyAnalysis
-
 import PhysiologyAnalysis: readABF, parseABF
+import PhysiologyAnalysis: DataFrame
 #%% Add the ability to flag files and remove them from the analysis
-data_file = raw"C:\Users\mtarc\OneDrive - The University of Akron\Projects\Retinoschisis\data_analysis.xlsx" #The data root
-dataset = openDataset(data_file, sheetName="all", typeConvert = true)
+datafile = raw"C:\Users\mtarc\OneDrive - The University of Akron\Projects\Retinoschisis\data_analysis.xlsx" #The data root
+dataset = openDataset(datafile, sheetName="all", typeConvert = true)
+dataset["STATS"] = dataset_statistics(dataset["EXPERIMENTS"])
+saveDataset(dataset, datafile)
+backupDataset(datafile)
