fda51c8f2453fed96c9e3c442567a9002d0d5175
mattar13 <mtarchick13@gmail.com>
2023-03-06 23:14:23 -0500

    added documentation to Models.jl

--- src/Fitting/Models.jl
+++ src/Fitting/Models.jl
@@ -156,7 +156,16 @@ RMS_Contrast(I::Array{T, 2}; normalized = true) where T <: Real = (1/(size(I,1)*
 
 #%% Lets write some fitting equations into the 
 #=========================== The below functions are created by fitting a model ===========================#
-function IRfit(intensity::AbstractArray{T}, response::AbstractArray{T};
+"""
+    function IRfit(intensity::AbstractArray{T}, response::AbstractArray{T};
+        lb::AbstractArray{T} = [1.0, 1.0, 0.1], #Default rmin = 100, kmin = 0.1, nmin = 0.1 
+        p0::AbstractArray{T} = [500.0, 1000.0, 2.0], #Default r = 500.0, k = 200.0, n = 2.0
+        ub::AbstractArray{T} = [Inf, Inf, 10.0], #Default rmax = 2400, kmax = 800
+    ) where {T<:Real}
+
+This function takes X and Y values and fits them according to a HILL type fit
+"""
+function HILLfit(intensity::AbstractArray{T}, response::AbstractArray{T};
     lb::AbstractArray{T} = [1.0, 1.0, 0.1], #Default rmin = 100, kmin = 0.1, nmin = 0.1 
     p0::AbstractArray{T} = [500.0, 1000.0, 2.0], #Default r = 500.0, k = 200.0, n = 2.0
     ub::AbstractArray{T} = [Inf, Inf, 10.0], #Default rmax = 2400, kmax = 800
