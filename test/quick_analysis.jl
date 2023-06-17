f1dd4788cbdb8fd0c8e97df5a37195134c080c88
mattar13 <mtarchick13@gmail.com>
2023-06-16 21:31:01 -0400

    updated quick analysis

--- test/quick_analysis.jl
+++ test/quick_analysis.jl
@@ -1,21 +1,21 @@
 using ElectroPhysiology, PhysiologyAnalysis
 
-data_root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis" #The data root
-all_files = data_root |> parseABF
-dataset["ALL_FILES"]
-dataset = createDataset(all_files, verbose = true)
+#Opening and reanalyzing a previously analyzed file
+data_root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal" #The data root
+
+#%% Opening a previously existant file
+data_file = joinpath(data_root, "data_analysis.xlsx") #This is the main file we can use. The root may change
+dataset = openDataset(data_file)
+
+#%% opening a file from new
+files = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2022_12_20_WTAdult\Mouse1_WT_Adult"|>parseABF
+dataset = createDataset(files, verbose = true)
+
 dataset = runTraceAnalysis(dataset, verbose = true)
-dataset = runExperimentAnalysis(dataset, verbose = true)
-datafile = raw"C:\Users\mtarc\OneDrive - The University of Akron\Projects\Retinoschisis\data_analysis.xlsx"
-
-#%%
-using DataFrames, Query
-using ElectroPhysiology, PhysiologyAnalysis
-datafile = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\data_analysis.xlsx" #The data root
-dataset = openDataset(datafile)
-size(dataset["EXPERIMENTS"])
-dataset["EXPERIMENTS"] |> @filter(_.Month == "12" && _.Date == "20" && _.Number == "1") |> DataFrame
-dataset["EXPERIMENTS"] |> @filter(_.Age == "Adult" && _.Genotype == "WT" && _.Month == "04") |> DataFrame
-
 dataset = runExperimentAnalysis(dataset)
-saveDataset(dataset, datafile)
+dataset = runConditionsAnalysis(dataset)
+backupDataset(data_file)
+saveDataset(dataset, data_file)
+
+
+dataset = createDataset(files, verbose = true)
