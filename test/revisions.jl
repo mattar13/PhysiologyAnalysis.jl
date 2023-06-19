5200c686ff40648d7c1760b469ddd0d26fe1f645
mattar13 <mtarchick13@gmail.com>
2023-06-19 10:26:50 -0400

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -1,34 +1,8 @@
+using Dates
 using Revise
 using PhysiologyAnalysis
-
-
 using ElectroPhysiology
-testFilter = raw"test/to_filter.abf"
-data = readABF(testFilter) |> filter_data
-
-
-#%%
-using PyPlot
-PyPlot.pygui(true)
-import PhysiologyAnalysis: readABF, parseABF
-import PhysiologyAnalysis: DataFrame
-using DataFrames, Query, XLSX
-
-data_root = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis" #The data root |> parseABF
-datafile = "C:\\Users\\mtarc\\OneDrive - The University of Akron\\Projects\\Retinoschisis\\data_analysis.xlsx"
-dataset = openDataset(datafile)
-dataset["CONDITIONS"] = summarize_data(dataset)
-dataset["STATS"] = dataset_statistics(dataset)
-backupDataset(datafile)
-saveDataset(dataset, datafile)
-
-plot_dataset_fits(dataset, normalize = false, condition = "NoDrugs")
-plot_dataset_vals(dataset)
-
-#test quick plot
-file = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2023_02_23_MattR141C\Mouse2_Adult_R141C\BaCl_LAP4\Rods"
-data = readABF(file |> parseABF) |> data_filter
-plot_experiment(data, xlims = (-0.25, 1.0))
+using DataFrames, Query
 
 #%% Make a new function for adding photon numbers to a flash stimulus
 using PhysiologyAnalysis
