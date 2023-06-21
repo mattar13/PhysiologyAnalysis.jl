002c71b68cd7f374445facbad939ac6503142482
mattar13 <mtarchick13@gmail.com>
2023-06-21 17:41:13 -0400

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,6 @@ using Test
 test_file = raw"to_analyze.abf"
 data = readABF(test_file) |> data_filter
 
-@testset "Testing ElectroPhysiology" begin
-    @test !isnothing(data)
-    @test isa(data, ElectroPhysiology.Experiment)
-end
-
 @testset "Testing ERG analysis" begin
-    resps = saturated_response(data)
-    #println(maximum(resps))
-    #@test maximum(resps) == -0.2988254587666856
+    include("testAnalysis.jl")
 end
--- test/testAnalysis.jl
+++ test/testAnalysis.jl
@@ -1,21 +1,22 @@
 #finding the saturated response
-print("Finding the saturated responses... ")
-@time resps = saturated_response(data);
-rmaxes = maximum(resps, dims=1);
+print("Testing the saturated response... ")
+resps = saturated_response(data);
+@test size(resps, 1) == size(data,1)
+@test size(resps, 2) == size(data,3)
 
-println("Pull out the dim response")
+#rmaxes = maximum(resps, dims=1);
 
-print("Finding the minima to peak... ")
-@time m2p = minima_to_peak(data);
+#print("Finding the minima to peak... ")
+#@time m2p = minima_to_peak(data);
 
-print("Finding the time to peak")
-@time t2p = time_to_peak(data);
+#print("Finding the time to peak")
+#@time t2p = time_to_peak(data);
 
-print("Finding the dominant time constant... ")
-@time t_dom = percent_recovery_interval(data, rmaxes);
+#print("Finding the dominant time constant... ")
+#@time t_dom = percent_recovery_interval(data, rmaxes);
 
-print("Finding the integration time")
-@time int_time = integral(data);
+#print("Finding the integration time")
+#@time int_time = integral(data);
 
-println("Extracting some timescale data")
+#println("Extracting some timescale data")
 
