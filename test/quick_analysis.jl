0fc0320da857941ec9aa9befe72124f7aaa4eaf1
mattar13 <mtarchick13@gmail.com>
2023-06-21 17:52:31 -0400

    updated quick analysis

--- test/quick_analysis.jl
+++ test/quick_analysis.jl
@@ -3,12 +3,13 @@ using DataFrames, Query, XLSX
 using PhysiologyAnalysis
 
 #Opening and reanalyzing a previously analyzed file
-data_file = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\data_analysis.xlsx" #The data root
+data_file = save_loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\data_analysis.xlsx" #The data root
 dataset = openDataset(data_file)
 backupDataset(data_file) #Backup the datafile
 
 #%% opening a file from new
-files = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2022_07_11_Adult\Mouse1_Adult_WT\BaCl_LAP4\Rods"|>parseABF
+files = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2019_03_12_AdultWT"|>parseABF
+save_loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\2019_03_12_Analysis.xlsx"
 dataset = createDataset(files, verbose = true)
 
 #%% Conducting the analysis
@@ -18,6 +19,4 @@ dataset = runConditionsAnalysis(dataset)
 dataset = runStatsAnalysis(dataset)
 
 #%% Saving the data
-save_loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\2022_07_11_Analysis.xlsx"
-save_loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\data_analysis.xlsx"
 saveDataset(dataset, save_loc)
