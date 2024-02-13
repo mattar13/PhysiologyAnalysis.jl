796e78e25f45b6fab5f32729f1e950efc301546f
mattar13 <mtarchick13@gmail.com>
2024-02-12 18:50:06 -0800

    updated testing

--- test/revisions.jl
+++ test/revisions.jl
@@ -1,68 +1,63 @@
-using Dates
 using Revise
-using PhysiologyAnalysis
 using ElectroPhysiology
-using DataFrames, Query, XLSX
-PhysiologyAnalysis.calibration_path()
-PhysiologyAnalysis.set_calibration_path(raw"C:\Users\mtarc\The University of Akron\Renna Lab - General\Data\Calibrations\photon_lookup.xlsx")
+using PhysiologyAnalysis
+using Pkg; Pkg.activate("test")
+PhysiologyAnalysis.__init__()
+using FileIO, ImageView, Images
+using GLMakie
+import GLMakie.Axis
+using Flux
 
-#%% We want to pull out a specific experiment or trace from the dataframe
-data_file = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\data_analysis.xlsx" #The data root
-dataset = openDataset(data_file , verbose = true)
+#%% 
+root = raw"F:\Data\Calcium Images\2024_01_31_FRMD7_Cal590"
+file = "2024_01_31_ROI004.tif"
+fn = joinpath(root, file)
+data = readImage(fn)
+mov = get_all_frames(data)
 
-exps = dataset["EXPERIMENTS"] |> 
-     @filter(_.Condition == "BaCl_LAP4" &&_.Photoreceptor == "Rods") |> 
-     @filter(_.Age == "P11") |> 
-     @map({Date = _.Date, Number = _.Number, Channel = _.Channel, Genotype = _.Genotype}) |> 
-DataFrame
-RS_trace_G = dataset["TRACES"] |> @filter(_.Condition == "NoDrugs" &&_.Photoreceptor == "Rods") |> DataFrame
+xsize, ysize, zsize = size(mov)
+xlims = 1:xsize
+ylims = 1:ysize
+zlims = 1:zsize
+#Create an average for the frames
+zavg = (sum(mov, dims = 3) ./ size(mov, 3))[:,:,1]
 
-save_loc = raw"C:\Users\mtarc\OneDrive - The University of Akron\Journal Submissions\2023_05_24 Critical Timepoints in retinoschisis - Unknown Journal\Plot_Outputs"
-for exps_sel in eachrow(exps) 
-     name = "$(exps_sel.Date)_$(exps_sel.Number)_$(exps_sel.Channel)_$(exps_sel.Genotype)"
-     println(name)
-     #color_sel = color_dict[exps_sel.Genotype]
-     #println(color_sel)
-     #fig_sel = plot_timepoint(RS_trace_A, RS_trace_B, RS_trace_G, exps_sel; color = color_sel, 
-     #     alims = (-100, 100), 
-     #     blims = (-50, 200), 
-     #     glims = (-200, 50)     
-     #)
-     #fig_sel.savefig("$(save_loc)\\$name.png")
-     
-     #qA = matchExperiment(RS_trace_A, exps_sel)
-     #dataA = readABF(qA, channels = exps_sel.Channel) |> data_filter
-     #writeXLSX("$(save_loc)\\$(name)_A.xlsx", dataA, :analysis)
-     
-     #qB = matchExperiment(RS_trace_B, exps_sel)
-     #if !isempty(qB)
-     #     dataB = readABF(qB, channels = exps_sel.Channel) |> data_filter
-     #     writeXLSX("$(save_loc)\\$(name)_B.xlsx", dataB, :analysis)
-     #end
-     qG = matchExperiment(RS_trace_G, exps_sel)
-     #println(qG)
-     if !isempty(qG)
-          dataG = readABF(qG, channels = exps_sel.Channel) |> data_filter
-          writeXLSX("$(save_loc)\\$(name)_G.xlsx", dataG, :analysis)
-     end
-     #plt.close("all")
+#Minimize the zavg
+threshold = 0.5
+zproj = (sum(data.data_array, dims = 1) ./ size(data, 1))[1, :, 1]
+
+fig = Figure()
+ax1 = GLMakie.Axis(fig[1,1])
+heatmap!(ax1, xlims, ylims, zavg, colormap = Reverse(:algae))
+display(fig)
+save("test.png", fig)
+save("test_img.png", zavg)
+#%%
+recordROI(data, [1,2,3,4], 1)
+getROImask(data, 1)
+
+ROI1 = getROIarr(data, 1)
+
+#%%
+fig = GLMakie.Figure(size = (1500, 400))
+ax11 = GLMakie.Axis(fig[1,1])
+ax12 = GLMakie.Axis3(fig[1,2])
+ax21 = GLMakie.Axis(fig[2,1])
+ax22 = GLMakie.Axis3(fig[2,2])
+ax3 = GLMakie.Axis(fig[3,1])
+
+heatmap!(ax11, xlims, ylims, zavg, colormap = Reverse(:algae), colorrange = (0.0, 0.05))
+surface!(ax12, xlims, ylims, zavg, colormap = Reverse(:algae), colorrange = (0.0, 0.05))
+hm = heatmap!(ax21, xlims, ylims, mov[:,:,1], colormap = Reverse(:algae), colorrange = (0.0, 0.05))
+sf = surface!(ax22, xlims, ylims, mov[:,:,1], colormap = Reverse(:algae), colorrange = (0.0, 0.05))
+lines!(ax3, zproj)
+ln_spot = lines!(ax3, fill(0.0, 2), [minimum(zproj), maximum(zproj)], color = :black)
+
+record(fig, "ANIMATION.mp4", 2:size(mov,3)) do i 
+    println(i)
+    frame = mov[:,:,i]
+    ln_spot[1] = fill(i, 2)
+    hm[3] = frame
+    sf[3] = frame
 end
-
-#%% Make a function that prints out a portion of a dataset
-info = (Date = Date(2019, 09, 25), Number = "1")
-dataset["TRACES"]
-dataset
-
-inc_dataset = matchDataset(dataset, info)
-exc_dataset = excludeDataset(dataset, info)
-
-#Now we want to pull out all files in inc dataset
-reinc_dataset["ALL_FILES"].Path
-
-#%% Make a new function for adding photon numbers to a flash stimulus
-files = raw"C:\Users\mtarc\OneDrive - The University of Akron\Data\ERG\Retinoschisis\2019_03_12_AdultWT\Mouse1_Adult_WT\BaCl" |> parseABF
-dataset = createDataset(files, debug = true, verbose = true)
-data = readABF(files) |> data_filter
-photons = dataset["ALL_FILES"].Photons ./ dataset["ALL_FILES"].Stim_Time
-setIntensity(data, photons)
-getIntensity(data)
+display(fig)
