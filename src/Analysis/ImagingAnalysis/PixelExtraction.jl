bedf31c030c0e8b0da435273c10c6bfd8e1c6b1d
mattar13 <mtarchick13@gmail.com>
2024-02-12 18:50:24 -0800

    added Pixel Extraction to PhysiologyAnalysis.jl

--- src/Analysis/ImagingAnalysis/PixelExtraction.jl
+++ src/Analysis/ImagingAnalysis/PixelExtraction.jl
@@ -1,11 +1,13 @@
 collapse_dims(a) = dropdims(a, dims = (findall(size(a) .== 1)...,))
 
 function zProject(exp::Experiment{TWO_PHOTON, T}) where T <: Real 
-     px, py = exp.HeaderDict["framesize"]
-     z_proj = (sum(exp, dims = 2) / size(exp, 2))
-     return reshape(z_proj, (px, py))
- end
+    px, py = exp.HeaderDict["framesize"]
+    z_proj = (sum(exp, dims = 2) / size(exp, 2))
+    return reshape(z_proj, (px, py))
+end
  
- frameAverage(data::Experiment{TWO_PHOTON, T}) where T<: Real = sum(exp, dims = 1)/size(exp,1)
+frameAverage(data::Experiment{TWO_PHOTON, T}) where T<: Real = sum(exp, dims = 1)/size(exp,1)
 
- export zProject, frameAverage
+function binarize(img, threshold = 0.5)
+    return img .> threshold
+end
--- src/PhysiologyAnalysis.jl
+++ src/PhysiologyAnalysis.jl
@@ -13,8 +13,6 @@ using LsqFit #Used for fitting amplification, Intensity Response, and Resistance
 
 #= Packages used for Analyzing data ==================================#
 import Polynomials as PN #used for fitting and stats
-#@time using DataFrames, Query, XLSX #Load these extra utilites immediately
-import XLSX: readtable, readxlsx #Import XLSX commands
 
 using StatsBase #Used for mean, std, and SEM functions.
 using HypothesisTests
@@ -36,7 +34,7 @@ export AMP, AMPfit
 export curve_fit #curve fitting from LsqFit
 
 #2) Data anlysis ========================================================================#
-include("Analysis/ERGAnalysis.jl")
+include("Analysis/ERGAnalysis/ERGAnalysis.jl")
 #export calculate_basic_stats
 export saturated_response, dim_response
 export minima_to_peak, time_to_peak
@@ -66,49 +64,77 @@ calibration_path() = read("$(homepath)/Datasheets/calibration.txt", String)
 #3) Import all Datasheet tools ===========================================================#
 function __init__()
      @require OrdinaryDiffEq = "1dea7af3-3e70-54e6-95c3-0bf5283fa5ed" begin
+          using .OrdinaryDiffEq
           @require ModelingToolkit = "961ee093-0014-501f-94e3-6117800e7a78" begin
                println("Loading differential equations and nose fitting")
-               using ModelingToolkit
-               using OrdinaryDiffEq
+               using .ModelingToolkit
                include("Fitting/NoseModel.jl")
                export findNosePeak
           end
      end
 
+     #Eventually we should massively restructure this
      @require DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0" begin
-          using DataFrames
-          using Query, XLSX
-          
-          println(Query)
-          
-          println(XLSX)
-          
-          export readtable, readxlsx, XLSX
-          include("Datasheets/RegexFunctions.jl")
+          using .DataFrames
+          #println("Dataframes exported)
+          @require Query = "1a8c2f83-1ff3-5112-b086-8aa67b057ba1" begin
+               using .Query 
+               #println(Query Exported)
+               @require XLSX = "fdbf4ff8-1666-58a4-91e7-1b58723a45e0" begin
+                    #println(XLSX exported)
+                    println("Loading dataframes and file opening")
+                    using .XLSX
+                    
+                    export readtable, readxlsx, XLSX
+                    include("Datasheets/RegexFunctions.jl")
 
-          include("Datasheets/FilePathExtraction.jl")
+                    include("Datasheets/FilePathExtraction.jl")
 
-          include("Datasheets/DatasheetFunctions.jl")
-          export matchDataset, excludeDataset, concatDatasets
-          export analyzeXLSX
-          
-          include("Datasheets/DatasheetCreation.jl")
-          export openDataset, createDataset
-          export saveDataset, backupDataset
+                    include("Datasheets/DatasheetFunctions.jl")
+                    export matchDataset, excludeDataset, concatDatasets
+                    export analyzeXLSX
+                    
+                    include("Datasheets/DatasheetCreation.jl")
+                    export openDataset, createDataset
+                    export saveDataset, backupDataset
 
-          include("Datasheets/DatasheetAnalysis.jl")
-          export runDataAnalysis
-          export runTraceAnalysis
-          export runExperimentAnalysis
-          export runConditionsAnalysis
-          export runStatsAnalysis
-          export matchExperiment, excludeExperiment
-          export flagExperiment, flagExperiment!, unflagALL!
-          export parseColumn!
-          export GenerateFitFrame
+                    include("Datasheets/DatasheetAnalysis.jl")
+                    export runDataAnalysis
+                    export runTraceAnalysis
+                    export runExperimentAnalysis
+                    export runConditionsAnalysis
+                    export runStatsAnalysis
+                    export matchExperiment, excludeExperiment
+                    export flagExperiment, flagExperiment!, unflagALL!
+                    export parseColumn!
+                    export GenerateFitFrame
 
-          include("Datasheets/extra_utilities.jl")
+                    include("Datasheets/extra_utilities.jl")
+               end
+          end
      end
+
+     @require FileIO = "5789e2e9-d7fb-5bc7-8068-2c6fae9b9549" begin
+          using .FileIO
+          @require Images = "916415d5-f1e6-5110-898d-aaa5f9f070e0" begin
+              using .Images
+              @require ImageView = "86fae568-95e7-573e-a6b2-d8a6b900c9ef" begin
+                  using .ImageView
+                  include("Analysis/ImagingAnalysis/PixelExtraction.jl")
+                  println("Imported necessary things")
+                  export zProject, frameAverage 
+                  export normalize, binarize
+              end
+          end
+     end
+
+     @require Flux = "587475ba-b771-5e3f-ad9e-33799f191a9c" begin
+          using .Flux
+          include("Analysis/ImagingAnalysis/MachineLearning.jl")
+          export build_model
+     end
+
+     #We want to add something for PyCall 
 end
 
 end
