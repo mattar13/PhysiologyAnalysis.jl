2d41e9199e8677e3d1147c369d4f47d463ccc7bc
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-06-22 11:44:17 -0700

    updated revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -4,14 +4,16 @@ using PhysiologyAnalysis
 using Pkg; Pkg.activate("test")
 #When we get to the dataframe creation we can fill in details here
 using XLSX, DataFrames, Query
-
+using FileIO, Images, ImageView, ImageMagick
 import .PhysiologyAnalysis.traverse_root
 
 patch_dir = raw"D:\Data\Patching"
 CaImg_dir = raw"D:\Data\Calcium Imaging"
 patch_files = traverse_root(patch_dir)
 twoP_files = traverse_root(CaImg_dir)
+foreach(println, patch_files)
+PhysiologyAnalysis.__init__()
 
-#I want to extract the date from .abf files
-data_test = ElectroPhysiology.readABFInfo(patch_files[1])
-#Now we pull out FileStartDateTime for each .abf file
+
+date_test = readABF(patch_files[1])
+img_test = readImage(twoP_files[1])
