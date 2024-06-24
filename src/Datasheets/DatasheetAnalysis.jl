6d2c1e56798096df50707bcd0dbff83dfed59aef
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-06-24 09:23:19 -0700

    updated DatasheetAnalysis

--- src/Datasheets/DatasheetAnalysis.jl
+++ src/Datasheets/DatasheetAnalysis.jl
@@ -1,17 +1,23 @@
-function pair_experiments(patch_datasheet, img_datasheet; tolerance = 100.0)
+function pair_experiments(patch_datasheet, img_datasheet;            
+    vmin = 5.30,
+    vmax = 6.30
+)
     paired_experiments = DataFrame(
         "ABF Experiment" => String[], 
         "TIF Experiment" => String[],
         "ABF Date" => DateTime[],
         "TIF Date" => DateTime[]
     )
-    for (i, date) in enumerate(patch_datasheet[:, "date_created"])
-        #subtract the entirety of the tif_datetime
-        elapsed_dates = abs.(date .- img_datasheet[:, "date_created"])
-        val, idx = findmin(elapsed_dates)
-        val_seconds = Dates.value(val)/1000
-        if val_seconds < tolerance
-            push!(paired_experiments, (patch_datasheet[i, "filename"],  img_datasheet[idx, "filename"], date, img_datasheet[idx, "date_created"]))
+    for (i, row) in enumerate(eachrow(patch_datasheet))
+        if !occursin("IV", row.Protocols)
+            date = row.date_created
+            elapsed_dates = (Dates.value.(abs.(date.- img_datasheet[:, "date_created"]))./1000)./60
+
+            idx = findlast(vmin .< elapsed_dates .<= vmax)
+
+            if !isnothing(idx)
+                push!(paired_experiments, (patch_datasheet[i, "filename"],  img_datasheet[idx, "filename"], date, img_datasheet[idx, "date_created"]))
+            end
         end
     end
     paired_experiments
--- test/revisions.jl
+++ test/revisions.jl
@@ -1,4 +1,5 @@
 using Revise
+using Dates
 using ElectroPhysiology 
 using PhysiologyAnalysis
 using Pkg; Pkg.activate("test")
@@ -7,13 +8,28 @@ using XLSX, DataFrames, Query
 using FileIO, Images, ImageView, ImageMagick
 import .PhysiologyAnalysis.traverse_root
 
+#%% Open all dates for the imaging files
+img_dir = raw"D:\Data\Calcium Imaging"
 patch_dir = raw"D:\Data\Patching"
-CaImg_dir = raw"D:\Data\Calcium Imaging"
-patch_files = traverse_root(patch_dir)
-twoP_files = traverse_root(CaImg_dir)
-foreach(println, patch_files)
+
+#Don't often need to do this (eventually we need to add an update button)
+#dataset = create2PDataSheet(img_dir, patch_dir; verbose = true)
+
+filename = raw"D:\Data\Analysis\data_analysis.xlsx"
+dataset = open2PDataSheet(filename)
+all_datasheet = dataset["All Files"]
+img_datasheet = dataset["TIF Files"]
+patch_datasheet = dataset["ABF Files"] 
+#I would like to iterate through each patch_file and indicate whether or not it is a IV curve
+protocols = []
+for (i, row) in enumerate(eachrow(patch_datasheet))
+    println("File $i")
+    HeaderDict = ElectroPhysiology.readABFInfo(row.filename)
+    push!(protocols, HeaderDict["ProtocolPath"])
+end
+protocols
+patch_datasheet.Protocols = protocols
+
 PhysiologyAnalysis.__init__()
-
-
-date_test = readABF(patch_files[1])
-img_test = readImage(twoP_files[1])
+dataset["Paired Files"] = pair_experiments(patch_datasheet, img_datasheet)
+save2PDataSheet(filename, dataset)
