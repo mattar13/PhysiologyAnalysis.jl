574b48ef7b0195c1a0a90aa269a3658b99bd8f52
mtarchick13@gmail.com <mtarchick13@gmail.com>
2024-06-24 23:39:06 -0700

    revisions

--- test/revisions.jl
+++ test/revisions.jl
@@ -11,25 +11,18 @@ import .PhysiologyAnalysis.traverse_root
 #%% Open all dates for the imaging files
 img_dir = raw"D:\Data\Calcium Imaging"
 patch_dir = raw"D:\Data\Patching"
+filename = raw"D:\Data\Analysis\data_analysis.xlsx"
 
 #Don't often need to do this (eventually we need to add an update button)
 #dataset = create2PDataSheet(img_dir, patch_dir; verbose = true)
 
-filename = raw"D:\Data\Analysis\data_analysis.xlsx"
+PhysiologyAnalysis.__init__()
 dataset = open2PDataSheet(filename)
 all_datasheet = dataset["All Files"]
 img_datasheet = dataset["TIF Files"]
 patch_datasheet = dataset["ABF Files"] 
-#I would like to iterate through each patch_file and indicate whether or not it is a IV curve
-protocols = []
-for (i, row) in enumerate(eachrow(patch_datasheet))
-    println("File $i")
-    HeaderDict = ElectroPhysiology.readABFInfo(row.filename)
-    push!(protocols, HeaderDict["ProtocolPath"])
-end
-protocols
-patch_datasheet.Protocols = protocols
-
-PhysiologyAnalysis.__init__()
 dataset["Paired Files"] = pair_experiments(patch_datasheet, img_datasheet)
 save2PDataSheet(filename, dataset)
+
+
+#%% Can we do an analysis for the IV curves
