0abc8cd94b570bd1c2ce6760950f1d8ebd292d68
mattar13 <mtarchick13@gmail.com>
2024-07-25 15:12:08 -0700

    updated cellpose port

--- src/Analysis/ImagingAnalysis/CellPose_port.jl
+++ src/Analysis/ImagingAnalysis/CellPose_port.jl
@@ -1,12 +1,20 @@
-#This is going to be a really difficult use of this function, but eventually would like to utilize 
-#Make this a standalone 
+using Revise
+using Dates
+using ElectroPhysiology 
+using PhysiologyAnalysis
+using Pkg; Pkg.activate("test")
+
 using PyCall
-using Conda
-
-#%% Create a new conda environment
-Conda.install("cellpose") #Run this to build the CellPose program
-
+# For some reason we need to do this weird dance. Just pyimport doesn't work and just python doesn't either
+path_loc = splitpath(pathof(PhysiologyAnalysis))[1:end-1] |> joinpath
+#ENV["CELLPOSE_LOCAL_MODELS_PATH"] = "C:/Users/mtarc/.julia/dev/PhysiologyAnalysis/src/Analysis/ImagingAnalysis/CellPoseModels"
 py"""
-
-from cellpost import model
+import os
+os.environ["CELLPOSE_LOCAL_MODELS_PATH"] = "C:/Users/mtarc/.julia/dev/PhysiologyAnalysis/src/Analysis/ImagingAnalysis/CellPoseModels"
+import cellpose
+from cellpose import models
 """
+cellpose = pyimport("cellpose")
+model = cellpose.models.Cellpose(model_type="cyto")
+
+model
