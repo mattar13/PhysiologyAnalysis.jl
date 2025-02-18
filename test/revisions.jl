c32fc9e09bcda772f1dc1837f0477bd226a7972f
mattar13 <mtarchick13@gmail.com>
2025-02-18 14:06:54 -0800

    update test/revisions.jl to modify file path, streamline baseline calculations, and enhance ROI processing

--- test/revisions.jl
+++ test/revisions.jl
@@ -4,18 +4,14 @@ using ElectroPhysiology, PhysiologyAnalysis
 
 #%% ╔═╡This task is for extraction of points, centroids, and ROIs using cellpose
 
-fn = raw"G:\Data\Two Photon\2025-02-12-GRAB-DA\grab-da-r2_da-puff10mM014.tif"
+fn = raw"G:\Data\Two Photon\2025-02-14-GRAB-DA\GRAB-DA2m-R1-R_da_puff_100um004.tif"
 data2P = readImage(fn);
 #deinterleave!(data2P) #This seperates the movies into two seperate movies
-
 img_frames = get_all_frames(data2P)
-pixel_splits(size(img_frames)[1:2], 15)
 
 time = data2P.t
 og_y = mean(img_frames[:,:,:,1], dims = (1,2))[1,1,:]
-baseline_y = baseline_als(og_y)
-include("../src/Analysis/ImagingAnalysis/NanoImg.jl")
-baseline_ma_y = baseline_ma(og_y-baseline_y)
+dFoF = baseline_trace(og_y)
 
 #%%
 using Pkg; Pkg.activate("test")
@@ -24,8 +20,10 @@ using GLMakie, PhysiologyPlotting
 fig = Figure()
 ax1 = Axis(fig[1, 1])
 lines!(ax1, time, og_y, color = :blue)
-lines!(ax1, time, baseline_y, color = :red)
-lines!(ax1, time, baseline_ma_y, color = :green)
 ax2 = Axis(fig[2, 1])
-lines!(ax1, time, og_y - baseline_y, color = :magenta)
+lines!(ax2, time, dFoF, color = :magenta)
 fig
+
+#%% Now work on the ROI sections
+#These parts are now from DeltaFF
+pixel_splits(size(img_frames)[1:2], 15)
