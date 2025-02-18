95e59b497660e643bbcf20cc8287d345a3d16fad
mattar13 <mtarchick13@gmail.com>
2025-02-18 14:03:10 -0800

    add pixel_splits function for determining pixel splitting indices in ROI analysis

--- src/Analysis/ImagingAnalysis/ROIAnalysis.jl
+++ src/Analysis/ImagingAnalysis/ROIAnalysis.jl
@@ -52,3 +52,24 @@ function findROIcentroids(array::Array{Int,2}, xlim::AbstractRange, ylim::Abstra
 end
 
 findROIcentroids(exp) = findROIcentroids(getROImask(exp), exp.HeaderDict["xrng"], exp.HeaderDict["yrng"])
+
+#These functions are used to do some pixel extraction and ROI analysis
+"""
+    pixel_splits(image_size::Tuple{Int, Int}, roi_size::Int) -> Tuple{Vector{Int}, Vector{Int}}
+
+Determines the pixel splitting indices for the image based on `roi_size`.
+"""
+function pixel_splits(image_size::Tuple{Int, Int}, roi_size::Int)
+    x_pixels, y_pixels = image_size
+    xs = collect(0:roi_size:x_pixels)
+    ys = collect(0:roi_size:y_pixels)
+
+    if xs[end] < x_pixels
+        push!(xs, x_pixels)
+    end
+    if ys[end] < y_pixels
+        push!(ys, y_pixels)
+    end
+
+    return (xs, ys)
+end
