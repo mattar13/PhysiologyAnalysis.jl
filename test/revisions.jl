d1c9482cb7743e1e3d1ca90329ca707749fc7fb8
mattar13 <mtarchick13@gmail.com>
2025-02-18 15:37:28 -0800

    enhance dFoF calculation in revisions.jl with moving average stack and pixel processing

--- test/revisions.jl
+++ test/revisions.jl
@@ -8,6 +8,19 @@ fn = raw"G:\Data\Two Photon\2025-02-14-GRAB-DA\GRAB-DA2m-R1-R_da_puff_100um004.t
 data2P = readImage(fn);
 #deinterleave!(data2P) #This seperates the movies into two seperate movies
 img_frames = get_all_frames(data2P)
+img_stack = data2P.data_array[:,:,1]
+#Calculate the stack dFoF
+#moving average stack
+
+import PhysiologyAnalysis.moving_average
+dFoF = baseline_stack(img_stack)
+
+dFoF |> size
+
+using OffsetArrays
+OffsetArrays.centered(ones(10,1))
+
+#%%
 
 time = data2P.t
 og_y = mean(img_frames[:,:,:,1], dims = (1,2))[1,1,:]
@@ -26,4 +39,6 @@ fig
 
 #%% Now work on the ROI sections
 #These parts are now from DeltaFF
-pixel_splits(size(img_frames)[1:2], 15)
+
+#Go through all pixels and calculate the dFoF
+
