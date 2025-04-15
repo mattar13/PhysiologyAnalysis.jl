0412e7153b405960e463e3e9c517d4e1828e8ea4
mtarchick13@gmail.com <mtarchick13@gmail.com>
2025-04-14 22:50:16 -0700

    fix: update file paths and enhance stimulus handling in revisions.jl

--- test/revisions.jl
+++ test/revisions.jl
@@ -5,10 +5,14 @@ using Statistics
 
 #%% ╔═╡This task is for extraction of points, centroids, and ROIs using cellpose
 
-fn = raw"G:\Data\Two Photon\2025-02-14-GRAB-DA\GRAB-DA2m-R1-R_da_puff_100um004.tif"
-ic_fn = raw"G:\Data\Patching\2025-02-14-da_puffs\25214001.abf"
+fn = raw"E:\Data\Two Photon\2025-02-14-GRAB-DA\GRAB-DA2m-R1-R_da_puff_100um004.tif"
+stim_fn = raw"E:\Data\Patching\2025-02-14-da_puffs\25214001.abf"
+
+#fn = raw"F:\Data\Two Photon\2025-02-26-GRAB-DA\grab-da-R1-R-nirCAT-70um_kcl_AP-NOMF-GABA009.tif"
+#stim_fn = raw"F:\Data\Patching\2025-02-26-da_puff\25226011.abf"
+
 data2P = readImage(fn);
-addStimulus!(data2P, ic_fn, "IN 2")
+addStimulus!(data2P, stim_fn, "IN 2", flatten_episodic = true)
 time2P = data2P.t
 
 #deinterleave!(data2P) #This seperates the movies into two seperate movies
@@ -29,18 +33,22 @@ lines!(ax1, time2P, df_trace, color = :black)
 vlines!(ax1, getStimulusEndTime(data2P), color = :red)
 
 fig
+
 #%% Now work on the ROI fitting. 
 #We want to pull out a single section from the stimulus
 
 idx = 3
-idx_start = round(Int64, getStimulusEndTime(data2P)[idx]/data2P.dt)
+getStimulusEndTime(data2P)
+idx_start = round(Int64, getStimulusEndTime(data2P)[idx]/data2P.dt)-50
 idx_end = round(Int64, getStimulusStartTime(data2P)[idx+1]/data2P.dt)
 segment_t = time2P[idx_start:idx_end]
-segment_trace = df_trace[idx_start:idx_end]
+segment_trace = dFoF[6000, idx_start:idx_end]
 
 fit = fit_parametric(segment_trace, segment_t)
 y_fit = map(TIME-> single_stim_model(TIME, fit.param), segment_t)
 fig, ax = lines(segment_t, segment_trace, color = :cyan)
 lines!(ax, segment_t, y_fit, color = :black)
+fig
 
+fit.param
 fit.resid
