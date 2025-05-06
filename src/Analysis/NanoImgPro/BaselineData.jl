f0270403d1c1fcab2c80667083c968fe624d66bd
mattar13 <mtarchick13@gmail.com>
2025-05-06 14:07:11 -0700

    feat: implement baseline correction function in ROIProcess.jl moved from BaselineData

--- src/Analysis/NanoImgPro/BaselineData.jl
+++ src/Analysis/NanoImgPro/BaselineData.jl
@@ -65,34 +65,3 @@ function moving_average(a::Vector{T}; window::Int = 15) where T<:Real
 
     return mavg
 end
-
-
-"""
-Perform overall baseline correction using ALS and centered Moving Average.
-
-- `stim_frame=0`: Disables stimulus-based normalization.
-- `ma_tail_start=0`: Disables ignored region in the moving average correction.
-- `window`: Size of the moving average window.
-
-Returns a baseline-corrected dF/F trace.
-"""
-function baseline_trace(trace::AbstractVector{T}; 
-    window::Int=15, #This is the window of the moving average for dF
-    kwargs...
-) where T<:Real
-
-    # Normalize using pre-stimulus baseline if stim_frame is set
-    baseline_divisor = mean(trace)  # Default to global mean if no stimulus
-
-    F0 = trace ./ baseline_divisor
-
-    # Apply Asymmetric Least Squares (ALS) smoothing
-    drift = baseline_als(F0; kwargs...)
-    dF = F0 .- drift
-
-    dFoF = moving_average(dF; window = window)
-
-    # Compute final dF/F trace
-    #dFoF = baselined .- ma_base
-    return dFoF
-end
