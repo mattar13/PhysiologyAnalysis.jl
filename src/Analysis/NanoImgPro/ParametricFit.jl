003ba70624f8c5464f9cf86514a34127c7888eae
mattar13 <mtarchick13@gmail.com>
2025-05-06 17:27:33 -0700

    fix: reorder parameters in fit_parametric function for consistency

--- src/Analysis/NanoImgPro/ParametricFit.jl
+++ src/Analysis/NanoImgPro/ParametricFit.jl
@@ -10,16 +10,10 @@ end
 
 single_stim_model(t_series::Vector{T}, p) where T<:Real = map(TIME-> single_stim_model(TIME, p), t_series)
 
-function fit_parametric(y_data, t_data; 
+function fit_parametric(t_data, y_data; 
     p0 = [0.50, 100.0, 10.0, 20.0], ub = [1.0, 1000.0, 1000.0, 100.0], lb = [0.0, 0.0, 0.0, 10.0]
 )
     model(t_series, p) = map(TIME-> single_stim_model(TIME, p), t_series)
     fit = curve_fit(model, t_data, y_data, p0, upper=ub, lower = lb)
     return fit
-end
-
-
-function iterative_fit(exp::Experiment)
-
-
 end
