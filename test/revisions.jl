1899afad16ef7b01cd58773097fbe4951ba2ff06
mattar13 <mtarchick13@gmail.com>
2025-05-20 15:27:47 -0700

    fix: update version number to 0.6.38 and modify file paths in revisions.jl for data processing

--- test/revisions.jl
+++ test/revisions.jl
@@ -7,13 +7,24 @@ import ElectroPhysiology: Experiment, TWO_PHOTON
 
 #%% ╔═╡This task is for extraction of points, centroids, and ROIs using cellpose
 #We should look through the available files and see which ones fit
-img_fn = raw"F:\Data\Two Photon\2025-05-02-GRAB-DA-nirCAT-STR\grab-nircat-str-20hz-100uA001.tif"
-stim_fn = raw"F:\Data\Patching\2025-05-02-GRAB-DA-STR\25502000.abf"
+img_fn = raw"H:\Data\Two Photon\2025-05-02-GRAB-DA-nirCAT-STR\grab-nircat-str-kpuff_3x012.tif"
+stim_fn = raw"H:\Data\Patching\2025-05-02-GRAB-DA-STR\25502017.abf"
+
+data = open2Pdata(img_fn, 
+    ic_stim_filename = stim_fn, stimulus_name = "IN 2", 
+    split_channel = true, main_channel = :grn,
+    #stimulus_threshold = 0.5, spike_train = true,
+    post_event_time = 120.0
+)
+xlims = data["xlims"]
+ylims = data["ylims"]
+getStimulusEndTime(data["experiment"])
 
 data2P = readImage(img_fn);
 deinterleave!(data2P) #This seperates the movies into two seperate movies
+data2P
+spike_train = false
 
-spike_train = true
 if spike_train
     #If we have a electrical stimulus we need to do the spike train analysis
     addStimulus!(data2P, stim_fn, "IN 3", flatten_episodic = true, stimulus_threshold = 0.5)
@@ -26,11 +37,9 @@ else
     time2P = data2P.t
 end
 
-stim_protocol
 
 # Split the image into 8x8 pixel ROIs
 pixel_splits_roi!(data2P, 8)
-
 # Process all ROIs for channel 2 and stimulus 2
 roi_analysis = process_rois(data2P; 
     channels=[1, 2],           # Only process channel 2
