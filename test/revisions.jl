54f2bcb944b5f5cde4166b4274ccb1d7231fa0b1
mattar13 <mtarchick13@gmail.com>
2025-05-29 16:56:28 -0700

    refactor: update sig_traces initialization to support multidimensional arrays for improved data structure handling

--- test/revisions.jl
+++ test/revisions.jl
@@ -12,7 +12,6 @@ img_fn = raw"H:\Data\Two Photon\2025-05-15-GRAB-DA_STR\b6_grabda-nircat-300uA_pu
 stim_fn = raw"H:\Data\Patching\2025-05-15-GRAB-DA-STR\25515025.abf"
 
 data_quin_base = load_electric_data(img_fn, stim_fn)
-data_quin_base["sig_traces"][1][2]
 
 #%% create a multidimensional array
 n_rois = length(data_quin_base["sig_traces"][1][1])
@@ -20,7 +19,7 @@ n_stims = length(data_quin_base["sig_traces"][1])
 n_timepoints = length(data_quin_base["sig_traces"][1][1][1])
 n_channels = length(data_quin_base["sig_traces"])
 
-sig_traces = fill(nothing, n_stims, n_channels)
+sig_traces = fill(nothing, n_stims, n_timepoints, n_channels)
 
 for STIM in 1:n_stims
     for CHANNEL in 1:n_channels
