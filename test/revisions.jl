d6b5737ebee77717e3d9bc3ebc727a0fe7e759d9
mtarchick13@gmail.com <mtarchick13@gmail.com>
2025-05-30 00:47:58 -0700

    refactor: adjust red and green wavelength parameters in load_electric_data function for improved baseline correction consistency

--- test/revisions.jl
+++ test/revisions.jl
@@ -12,7 +12,7 @@ println("Loading the quinpirole baseline data...")
 img_fn = raw"F:\Data\Two Photon\2025-05-15-GRAB-DA_STR\b6_grabda-nircat-300uA_pulse016.tif"
 stim_fn = raw"F:\Data\Patching\2025-05-15-GRAB-DA-STR\25515025.abf"
 
-data_quin_base = load_electric_data(img_fn, stim_fn, red_lam = 1e5, grn_lam = 1e3)
+data_quin_base = load_electric_data(img_fn, stim_fn, red_lam = 1e3, grn_lam = 1e3)
 
 all_rois = data_quin_base["sig_traces"][2][3]
 lines(all_rois[1])
