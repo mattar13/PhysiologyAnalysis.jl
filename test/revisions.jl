b0a4f737bbb610a794ac70eb0b539e0ce327a5f1
mattar13 <mtarchick13@gmail.com>
2025-06-03 14:37:59 -0700

    refactor: integrate open2Pdata function for streamlined data loading and visualization, including example plots and ROI analysis

--- test/revisions.jl
+++ test/revisions.jl
@@ -12,10 +12,36 @@ println("Loading the quinpirole baseline data...")
 img_da_10mM_fn = raw"H:\Data\Two Photon\2025-05-19-nirCAT-STR-DPUFF\nirCAT_s2_10mM_mq010.tif"
 stim_da_10mM_fn = raw"H:\Data\Patching\2025-05-19-nirCAT-str\25519016.abf"
 
-experiment = readImage(img_da_10mM_fn)
-stim_trace = readABF(stim_da_10mM_fn, flatten_episodic = true,stimulus_name = "IN 2", stimulus_threshold = 0.5)
-stimulusProtocol = getStimulusProtocol(stim_trace)
-addStimulus!(experiment, stimulusProtocol)
-stim_ends = getStimulusEndTime(experiment)
-dt = experiment.dt
+data = open2Pdata(img_da_10mM_fn, 
+    #Indicate the stimulus dataset
+    stim_filename = stim_da_10mM_fn, #New option if we want to specify a 2P stimulus dataset
+    stimulus_name = "IN 2", 
+    stimulus_threshold = 0.5,
+    
+    split_channel = false, 
+    main_channel = :red,
+    pre_event_time = 20.0, 
+    post_event_time = 60.0, 
 
+    verbose = 3, # 1: basic progress, 2: timing info, 3: detailed info
+)
+
+#%% ╔═╡Now make example plots to show the results
+fig = Figure(size = (1000, 500))
+
+ax1a = Axis(fig[1,1], title = "Original", xlabel = "Time (s)", ylabel = "dF/F")
+ax2a = Axis(fig[2,1], title = "dF/F", xlabel = "Time (s)", ylabel = "dF/F")
+
+ax1b = Axis(fig[1,2], title = "Original", xlabel = "Time (s)", ylabel = "dF/F")
+ax2b = Axis(fig[2,2], title = "dF/F", xlabel = "Time (s)", ylabel = "dF/F")
+
+lines!(ax1a, data["time"], data["grn_trace"], color = :green, label = "Green")
+lines!(ax2a, data["time"], data["dff_grn_trace"], color = :green, label = "Green")
+
+lines!(ax1b, data["time"], data["red_trace"], color = :red, label = "Red")
+lines!(ax2b, data["time"], data["dff_red_trace"], color = :red, label = "Red")
+
+fig
+
+#%% Now do the ROI analysis
+data = PhysiologyAnalysis.load_puffing_data(img_da_10mM_fn, stim_da_10mM_fn)
