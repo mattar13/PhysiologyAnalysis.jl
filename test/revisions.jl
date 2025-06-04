97f6d582e609679541a84c705852696c77502a54
mattar13 <mtarchick13@gmail.com>
2025-06-04 11:19:16 -0700

    fix: update image and stimulus file paths, adjust stimulus parameters, and enhance plotting for improved data visualization

--- test/revisions.jl
+++ test/revisions.jl
@@ -7,44 +7,43 @@ import ElectroPhysiology: Experiment, TWO_PHOTON
 using Pkg; Pkg.activate("test")
 using GLMakie, PhysiologyPlotting
 
-#%% ╔═╡This task is for extraction of points, centroids, and ROIs using cellpose
+#%% ╔═╡Found an issue. With really crazy spikes, the baseline correction is not working.
 println("Loading the quinpirole baseline data...")
-img_da_10mM_fn = raw"H:\Data\Two Photon\2025-05-19-nirCAT-STR-DPUFF\nirCAT_s2_10mM_mq010.tif"
-stim_da_10mM_fn = raw"H:\Data\Patching\2025-05-19-nirCAT-str\25519016.abf"
+img_fn = raw"H:\Data\Two Photon\2025-05-15-GRAB-DA_STR\b6_grabda-nircat-300uA_pulse_QUIN016.tif"
+stim_fn = raw"H:\Data\Patching\2025-05-15-GRAB-DA-STR\25515026.abf"
 
-data = open2Pdata(img_da_10mM_fn, 
+# img_fn = raw"H:\Data\Two Photon\2025-05-15-GRAB-DA_STR\b6_grabda-nircat-300uA_pulse016.tif"
+# stim_fn = raw"H:\Data\Patching\2025-05-15-GRAB-DA-STR\25515025.abf"
+
+data = open2Pdata(img_fn, 
     #Indicate the stimulus dataset
-    stim_filename = stim_da_10mM_fn, #New option if we want to specify a 2P stimulus dataset
-    stimulus_name = "IN 2", 
-    stimulus_threshold = 0.5,
+    stim_filename = stim_fn, #New option if we want to specify a 2P stimulus dataset
+    stimulus_name = "IN 3", 
+    spike_train = true,
     
-    split_channel = false, 
+    split_channel = true, 
     main_channel = :red,
     pre_event_time = 20.0, 
     post_event_time = 60.0, 
-
+    red_spike_reduction = :median,
+    grn_spike_reduction = :median,
+    red_window = 5,
+    grn_window = 5,
+    red_lam = 1e3,
+    red_assym = 0.005,
     verbose = 3, # 1: basic progress, 2: timing info, 3: detailed info
 )
-
-#%% ╔═╡Now make example plots to show the results
+# ╔═╡Now make example plots to show the results
 fig = Figure(size = (1000, 500))
 
 ax1a = Axis(fig[1,1], title = "Original", xlabel = "Time (s)", ylabel = "dF/F")
 ax2a = Axis(fig[2,1], title = "dF/F", xlabel = "Time (s)", ylabel = "dF/F")
 
-ax1b = Axis(fig[1,2], title = "Original", xlabel = "Time (s)", ylabel = "dF/F")
-ax2b = Axis(fig[2,2], title = "dF/F", xlabel = "Time (s)", ylabel = "dF/F")
+lines!(ax1a, data["time"], data["red_trace"]./mean(data["red_trace"]), color = :red, label = "Red")
+lines!(ax1a, data["time"], data["red_drift"], color = :blue, label = "Drift")
+vlines!(ax1a, data["tstamps"], color = :black, linestyle = :dash)
 
-lines!(ax1a, data["time"], data["grn_trace"], color = :green, label = "Green")
-lines!(ax2a, data["time"], data["dff_grn_trace"], color = :green, label = "Green")
-
-lines!(ax1b, data["time"], data["red_trace"], color = :red, label = "Red")
-lines!(ax2b, data["time"], data["dff_red_trace"], color = :red, label = "Red")
+lines!(ax2a, data["time"], data["dff_red_trace"], color = :green, label = "dF/F")
+vlines!(ax2a, data["tstamps"], color = :black, linestyle = :dash)
 
 fig
-
-#%% Now do the ROI analysis
-data = PhysiologyAnalysis.load_puffing_data(img_da_10mM_fn, stim_da_10mM_fn)
-
-data["pks"]
-data["grn_sect_arr"]
