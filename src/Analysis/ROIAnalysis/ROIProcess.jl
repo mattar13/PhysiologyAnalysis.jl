0c845cb84a6da5010e360e9d27d647062983c356
mattar13 <mtarchick13@gmail.com>
2025-06-19 14:41:44 -0700

    feat: enhance ROI processing by adding signal threshold calculations and improving parameter handling in process_rois function

--- src/Analysis/ROIAnalysis/ROIProcess.jl
+++ src/Analysis/ROIAnalysis/ROIProcess.jl
@@ -1,26 +1,75 @@
 function process_rois(data::Experiment{TWO_PHOTON, T}; 
-    stim_indices=nothing,
+    
+    #Parameters for the specific ROI target
+    channel=1,
+    roi_indices=nothing,
+
     #Baseline correction parameters
+    window::Int=40,
     baseline_divisor_start = 1,
     baseline_divisor_end = nothing,
     linear_fill_start = nothing,
     linear_fill_end = nothing,
+    
     #ROI parameters
-    channels=nothing,
-    roi_indices=nothing,
-    delay_time=50.0,
-    window::Int=15,
-    n_stds=2.0,
-    sig_window=50.0,  # Time window in ms to look for significant responses after stimulus
-    analysis_window_before=100.0,  # Time window in ms before stimulus to analyze
-    analysis_window_after=50.0,   # Time window in ms after stimulus to analyze
+    pos_sig_level = 2.0,
+    neg_sig_level = 3.0,
+
+    #These parameters are in terms of frames. Change for ms
+    stim_frame = 50, 
+    sig_threshold_std_start = 1,
+    sig_threshold_std_end = 5,
+    sig_threshold_mean_start = 12,
+    sig_threshold_mean_end = 2,
+    argmax_threshold_end = 25,
+    max_dfof_end = 5,
+    min_dfof_end = 100,
+
 ) where T<:Real
 
     roi_indices = getROImask(data) |> unique
+    sig_rois = falses(length(roi_indices))
+    dFoF_traces = zeros(length(roi_indices), length(data.t))
+
     for roi_idx in roi_indices
         println("Processing ROI $roi_idx")
+        roi_arr = getROIarr(data, roi_idx)
+        roi_trace = mean(roi_arr, dims = 1)[1,:, channel]
+        #Adjust this a bit to get a better
+        dFoF = baseline_trace(roi_trace, 
+            window = window, 
+            baseline_divisor_start = baseline_divisor_start, 
+            baseline_divisor_end = baseline_divisor_end, 
+            linear_fill_start = linear_fill_start, 
+            linear_fill_end = linear_fill_end)
+
+        #These are the parameters for the signal threshold
+        sig_std = std(dFoF[sig_threshold_std_start:stim_frame-sig_threshold_std_end])
+        sig_mean = mean(dFoF[stim_frame-sig_threshold_mean_start: stim_frame-sig_threshold_mean_end])
+        #println("sig_mean: $sig_mean, sig_std: $sig_std")
+        
+        #Calculate the threshold for the signal
+        sig_threshold = sig_mean + sig_std * pos_sig_level
+        neg_threshold = sig_mean - sig_std * neg_sig_level
+        #println("sig_threshold: $sig_threshold, neg_threshold: $neg_threshold")
+
+        trace_argmax = argmax(dFoF[stim_frame:stim_frame+argmax_threshold_end]) + stim_frame
+        
+        max_dfof = mean(dFoF[trace_argmax:trace_argmax+max_dfof_end])
+        min_dfof = minimum(dFoF[stim_frame:stim_frame+min_dfof_end])
+        over_max = max_dfof > sig_threshold
+        over_min = min_dfof > neg_threshold
+        if over_max && over_min
+            println("ROI $roi_idx is significant")
+            sig_rois[roi_idx] = true
+        end
+
+        #println("max_dfof: $max_dfof, min_dfof: $min_dfof, over_max: $over_max, over_min: $over_min ")
+
+        dFoF_traces[roi_idx, :] = dFoF
+    
     end
 
-
+    return sig_rois, dFoF_traces
 
 end
--- test/revisions.jl
+++ test/revisions.jl
@@ -18,7 +18,9 @@ stim_fn3 = raw"G:\Data\Patching\2025-05-15-GRAB-DA-STR\25515021.abf"
 data_img = readImage(img_fn3)
 deinterleave!(data_img)
 truncate_data!(data_img, t_begin = 0.0, t_end = 500.0)
-stimulus = readABF(stim_fn3)
+stimulus = readABF(stim_fn3, stimulus_name = "IN 3", stimulus_threshold = 0.5, flatten_episodic = true)
+spike_train_group!(stimulus, 1.0)
+addStimulus!(data_img, stimulus, "IN 3")
 
 function plot_baseline_trace(trace::AbstractVector{T}; 
     window::Int = 20,
@@ -34,28 +36,32 @@ function plot_baseline_trace(trace::AbstractVector{T};
     plot(p1, p2, layout = (2, 1))
 end
 
+#Demonstrate the ROI significance finding
+
 #%% Lets build the pixel splits function again (using minimal AI)
-pixel_splits_roi!(data_img, 10)
+pixel_splits_roi!(data_img, 8)
 mask = getROImask(data_img)
-
-
-
-
+stim_end_times = getStimulusEndTime(data_img)
+stim_end_idxs = round.(Int, stim_end_times ./ data_img.dt)
+sig_rois, dFoF_traces = process_rois(data_img, stim_frame = stim_end_idxs[2])
+mask_xy = round.(Int64, sqrt(size(sig_rois,1))) 
+heatmap(reshape(sig_rois, mask_xy, mask_xy))
 
 #%% We want to store the ROIs in a object
-make_circular_roi!(data_img, (75, 75), 75)
-heatmap(getROImask(data_img))
+#make_circular_roi!(data_img, (75, 75), 75)
 
-roi_trace = mean(getROIarr(data_img, 1), dims = 1)[1,:, 2]
+t_rng = data_img.t[1]:data_img.dt:data_img.t[end]
+roi_trace = mean(getROIarr(data_img, 50), dims = 1)[1,:, 2]
 
 roi_FILT = baseline_trace(roi_trace, 
-    window = 50, 
+    window = 40, 
     baseline_divisor_start = 1, baseline_divisor_end = 120,
     linear_fill_start = 120, linear_fill_end = 180
 )
 
-p1 = plot(roi_trace, label = "Raw")
-p2 = plot(roi_FILT, label = "Filtered")
+p1 = plot(t_rng, roi_trace, label = "Raw")
+p2 = plot(t_rng, roi_FILT, label = "Filtered")
+vline!(p2, getStimulusEndTime(data_img), color = :red, label = "Stimulus")
 plot(p1, p2, layout = (2, 1))
 
 #%%
