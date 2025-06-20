0777b9d66cf95dfc671f695988519e6ecc85f751
mattar13 <mtarchick13@gmail.com>
2025-06-19 17:06:03 -0700

    fix: update ROI processing parameters and enhance signal threshold logging in process_rois function

--- src/Analysis/ROIAnalysis/ROIProcess.jl
+++ src/Analysis/ROIAnalysis/ROIProcess.jl
@@ -1,12 +1,12 @@
 function process_rois(data::Experiment{TWO_PHOTON, T}; 
     
     #Parameters for the specific ROI target
-    channel=1,
+    channel=2,
     roi_indices=nothing,
 
     #Baseline correction parameters
     window::Int=40,
-    baseline_divisor_start = 1,
+    baseline_divisor_start = 2,
     baseline_divisor_end = nothing,
     linear_fill_start = nothing,
     linear_fill_end = nothing,
@@ -27,7 +27,9 @@ function process_rois(data::Experiment{TWO_PHOTON, T};
 
 ) where T<:Real
 
-    roi_indices = getROImask(data) |> unique
+    if isnothing(roi_indices)
+        roi_indices = getROImask(data) |> unique
+    end
     sig_rois = falses(length(roi_indices))
     dFoF_traces = zeros(length(roi_indices), length(data.t))
 
@@ -46,17 +48,19 @@ function process_rois(data::Experiment{TWO_PHOTON, T};
         #These are the parameters for the signal threshold
         sig_std = std(dFoF[sig_threshold_std_start:stim_frame-sig_threshold_std_end])
         sig_mean = mean(dFoF[stim_frame-sig_threshold_mean_start: stim_frame-sig_threshold_mean_end])
-        #println("sig_mean: $sig_mean, sig_std: $sig_std")
+        println("sig_mean: $sig_mean, sig_std: $sig_std")
         
         #Calculate the threshold for the signal
         sig_threshold = sig_mean + sig_std * pos_sig_level
         neg_threshold = sig_mean - sig_std * neg_sig_level
-        #println("sig_threshold: $sig_threshold, neg_threshold: $neg_threshold")
+        println("sig_threshold: $sig_threshold, neg_threshold: $neg_threshold")
 
         trace_argmax = argmax(dFoF[stim_frame:stim_frame+argmax_threshold_end]) + stim_frame
         
         max_dfof = mean(dFoF[trace_argmax:trace_argmax+max_dfof_end])
         min_dfof = minimum(dFoF[stim_frame:stim_frame+min_dfof_end])
+        println("max_dfof: $max_dfof, min_dfof: $min_dfof")
+
         over_max = max_dfof > sig_threshold
         over_min = min_dfof > neg_threshold
         if over_max && over_min
@@ -64,7 +68,6 @@ function process_rois(data::Experiment{TWO_PHOTON, T};
             sig_rois[roi_idx] = true
         end
 
-        #println("max_dfof: $max_dfof, min_dfof: $min_dfof, over_max: $over_max, over_min: $over_min ")
 
         dFoF_traces[roi_idx, :] = dFoF
     
