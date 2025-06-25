24cf42c5d346fbb02a0133e65d96ca65a7d113b4
mattar13 <mtarchick13@gmail.com>
2025-06-25 15:50:59 -0700

    refactor: update heatmap color scheme and line colors in ROI plots for improved visualization

--- test/runROIdemo.jl
+++ test/runROIdemo.jl
@@ -208,9 +208,9 @@ sig_arr_dFoF[:,2] = baseline_trace(sig_arr_zproj[:,2],
 
 # --- Plot 4: ROI Heatmap and Traces --- #
 ax3a = Axis(fig[1,2], title = "Significant ROI Mask", aspect = 1)
-heatmap!(ax3a, rotl90(sig_mask), colormap = :viridis)
+heatmap!(ax3a, reverse(sig_mask, dims = 2), colormap = :grays)
 ax3b = Axis(fig[2,2], title = "ROI Trace 1", xlabel = "Pixel Index", ylabel = "Mean Intensity")
-lines!(ax3b, sig_arr_dFoF[:,1], color = :blue)
+lines!(ax3b, sig_arr_dFoF[:,1], color = :green)
 ax3c = Axis(fig[3,2], title = "ROI Trace 2", xlabel = "Pixel Index", ylabel = "Mean Intensity")
 lines!(ax3c, sig_arr_dFoF[:,2], color = :red)
 
